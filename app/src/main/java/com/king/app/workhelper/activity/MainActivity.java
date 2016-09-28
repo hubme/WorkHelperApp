58649500db66e905b9af47f0ab12a6603169abef
VanceKing <huogxu@gmail.com>
2016-09-29 00:19:17 +0800

    [结构] 搭建项目架构

---
+++ app/src/main/java/com/king/app/workhelper/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.king.app.workhelper.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.king.app.workhelper.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/king/app/workhelper/app/WorkHelperApp.java
@@ -0,0 +1,15 @@
+package com.king.app.workhelper.app;
+
+import android.app.Application;
+
+/**
+ * 全局Application
+ * Created by VanceKing on 2016/9/28.
+ */
+
+public class WorkHelperApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
