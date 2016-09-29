7247899cb984d3f0dbafef88e3fbfa2b23c6d868
huoguangxu <huoguangxu@9188.com>
2016-09-29 11:26:53 +0800

    [微调] 1.修改主题为NoActionBar 2.修改build.gradle

--- app/src/main/java/com/king/app/workhelper/activity/MainActivity.java
+++ app/src/main/java/com/king/app/workhelper/activity/MainActivity.java
@@ -1,7 +1,7 @@
 package com.king.app.workhelper.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.king.app.workhelper.R;
 
