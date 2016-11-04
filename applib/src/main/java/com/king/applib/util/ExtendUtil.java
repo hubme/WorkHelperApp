f91091d879757d90fc6b86096655333d88f463e7
huoguangxu <huoguangxu@9188.com>
2016-11-04 16:28:04 +0800

    添加判空工具方法

--- applib/src/main/java/com/king/applib/util/ExtendUtil.java
+++ applib/src/main/java/com/king/applib/util/ExtendUtil.java
@@ -7,11 +7,17 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Environment;
+import android.util.SparseArray;
+import android.util.SparseBooleanArray;
+import android.util.SparseIntArray;
+import android.util.SparseLongArray;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 
 import com.king.applib.log.Logger;
 
+import java.lang.reflect.Array;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
@@ -24,7 +30,7 @@ import static android.content.Context.CLIPBOARD_SERVICE;
 
 public class ExtendUtil {
     private ExtendUtil() {
-
+        throw new UnsupportedOperationException("u can't instantiate me...");
     }
 
     public static boolean isListNullOrEmpty(List list) {
@@ -45,6 +51,45 @@ public class ExtendUtil {
             }
         }
 
+    }
+
+    /**
+     * 判断对象是否为空
+     *
+     * @param obj 对象
+     * @return {@code true}: 为空<br>{@code false}: 不为空
+     */
+    public static boolean isEmpty(Object obj) {
+        if (obj == null) {
+            return true;
+        }
+        if (obj instanceof String && obj.toString().length() == 0) {
+            return true;
+        }
+        if (obj.getClass().isArray() && Array.getLength(obj) == 0) {
+            return true;
+        }
+        if (obj instanceof Collection && ((Collection) obj).isEmpty()) {
+            return true;
+        }
+        if (obj instanceof Map && ((Map) obj).isEmpty()) {
+            return true;
+        }
+        if (obj instanceof SparseArray && ((SparseArray) obj).size() == 0) {
+            return true;
+        }
+        if (obj instanceof SparseBooleanArray && ((SparseBooleanArray) obj).size() == 0) {
+            return true;
+        }
+        if (obj instanceof SparseIntArray && ((SparseIntArray) obj).size() == 0) {
+            return true;
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+            if (obj instanceof SparseLongArray && ((SparseLongArray) obj).size() == 0) {
+                return true;
+            }
+        }
+        return false;
     }
 
     /**
