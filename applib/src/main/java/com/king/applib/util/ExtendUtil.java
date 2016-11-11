5f355e6322df9ffd70fb25bfe7b6f417607cd32d
huoguangxu <huoguangxu@youyuwo.com>
2016-11-11 17:13:15 +0800

    Toolbar

--- applib/src/main/java/com/king/applib/util/ExtendUtil.java
+++ applib/src/main/java/com/king/applib/util/ExtendUtil.java
@@ -57,7 +57,6 @@ public class ExtendUtil {
 
     /**
      * 判断对象是否为空
-     *
      * @param obj 对象
      * @return {@code true}: 为空<br>{@code false}: 不为空
      */
@@ -245,5 +244,17 @@ public class ExtendUtil {
         colorFilter = new ColorMatrixColorFilter(matrix);
 
         return colorFilter;
+    }
+
+    /**
+     * 获取状态栏的高度
+     */
+    public int getStatusBarHeight(Context context) {
+        int result = 0;
+        int resourceId = context.getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            result = context.getResources().getDimensionPixelSize(resourceId);
+        }
+        return result;
     }
 }
