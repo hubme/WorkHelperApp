525d19543e7db76cb10c43ad46bf8514f34e395e
huoguangxu <huoguangxu@youyuwo.com>
2016-11-15 19:47:58 +0800

    蒙层控件

--- app/src/main/java/com/king/app/workhelper/activity/CustomViewActivity.java
+++ app/src/main/java/com/king/app/workhelper/activity/CustomViewActivity.java
@@ -2,6 +2,10 @@ package com.king.app.workhelper.activity;
 
 import com.king.app.workhelper.R;
 import com.king.app.workhelper.common.AppBaseActivity;
+import com.king.app.workhelper.ui.customview.LayerImageView;
+
+import butterknife.BindView;
+import butterknife.OnClick;
 
 /**
  * 自定义View
@@ -10,6 +14,9 @@ import com.king.app.workhelper.common.AppBaseActivity;
 
 public class CustomViewActivity extends AppBaseActivity {
 
+    @BindView(R.id.layer_image)
+    public LayerImageView mLayerImageView;
+
     @Override
     public int getContentLayout() {
         return R.layout.activity_custom;
@@ -17,6 +24,16 @@ public class CustomViewActivity extends AppBaseActivity {
 
     @Override protected void initData() {
         super.initData();
+        mLayerImageView.setImageResource(R.mipmap.calc_guide_head);
+    }
 
+    @OnClick(R.id.layer_image)
+    public void clickLayer() {
+        mLayerImageView.setImageResource(R.drawable.little_boy_01);
+        mLayerImageView.showLayer();
+        mLayerImageView.setLayerColor(R.color.light_orange_ff8800);
+        mLayerImageView.setLayerHint("了反思");
+        mLayerImageView.setHintSize(R.dimen.ts_huge);
+        mLayerImageView.setHintColor(R.color.colorPrimaryDark);
     }
 }
