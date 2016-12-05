ca6f9e605d766b21d86c2b5a16f0cdeb0c4aab18
huoguangxu <huoguangxu@9188.com>
2016-12-05 19:37:26 +0800

    [功能] Toolbar的使用

---
+++ app/src/main/java/com/king/app/workhelper/activity/ToolbarActivity.java
@@ -0,0 +1,10 @@
+package com.king.app.workhelper.activity;
+
+import com.king.app.workhelper.common.AppBaseActivity;
+
+/**
+ * Created by HuoGuangxu on 2016/12/5.
+ */
+
+public class ToolbarActivity extends AppBaseActivity {
+}
