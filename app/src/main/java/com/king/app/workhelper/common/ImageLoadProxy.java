b2dc62841f8b04d7361532d90dcd5d32f60b5f78
VanceKing <huogxu@gmail.com>
2017-03-20 23:49:12 +0800

    [todo] 图片加载接口

---
+++ app/src/main/java/com/king/app/workhelper/common/ImageLoadProxy.java
@@ -0,0 +1,17 @@
+package com.king.app.workhelper.common;
+
+import android.widget.ImageView;
+
+/**
+ * 定义图片加载接口，隔离具体的实现，解耦。
+ *
+ * @author VanceKing
+ * @since 2017/3/20 0020.
+ */
+
+// TODO: 2017/3/20 完成图片加载接口
+public class ImageLoadProxy {
+    public static void displayImage(String url, ImageView target) {
+
+    }
+}
