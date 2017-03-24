69633a8fa3dee5b69852e2c770a5b30ddb63e8e1
VanceKing <huogxu@gmail.com>
2017-03-24 18:24:32 +0800

    [优化] SPUtil使用全局上下文

--- applib/src/androidTest/java/com/king/applib/SPUtilTest.java
+++ applib/src/androidTest/java/com/king/applib/SPUtilTest.java
@@ -1,20 +1,24 @@
 package com.king.applib;
 
+import com.king.applib.util.ContextUtil;
 import com.king.applib.util.ExtendUtil;
 import com.king.applib.util.SPUtil;
 
+import java.util.Arrays;
 import java.util.List;
-
-import static java.util.Arrays.asList;
 
 /**
  * @author huoguangxu
  * @since 2017/3/6.
  */
 
-public class SPUtilTest extends BaseTestCase{
+public class SPUtilTest extends BaseTestCase {
+    @Override protected void setUp() throws Exception {
+        super.setUp();
+        ContextUtil.init(mContext);
+    }
 
-    public static class TestOo{
+    public static class TestOo {
         public String name;
         public int age;
 
@@ -30,38 +34,37 @@ public class SPUtilTest extends BaseTestCase{
                     '}';
         }
     }
-    public void testPutStringList() throws Exception{
-        List<String> stringList = asList("哈哈哈", "呵呵呵", "哦哦哦");
-//        SPUtil.putStringList(mContext, "1111", stringList);
-        List<String> strings = stringList.subList(0, 1);
-        ExtendUtil.printList(strings);
+
+    public void testPutStringList() throws Exception {
+        List<String> stringList = Arrays.asList("哈哈哈", "呵呵呵", "哦哦哦");
+        SPUtil.putStringList("1111", stringList);
     }
-    
+
     public void testGetStringList() throws Exception {
-        List<String> stringList = SPUtil.getStringList(mContext, "1111");
+        List<String> stringList = SPUtil.getStringList("1111");
         ExtendUtil.printList(stringList);
     }
-    
+
     public void testPutListToSp() throws Exception {
-//        List<TestOo> listOo  = Arrays.asList(new TestOo("1111", 1111), new TestOo("2222", 2222), new TestOo("3333", 3333));
-//        SPUtil.putListToSp(mContext, "2222", listOo);
-//        SPUtil.putListToSp(mContext, "3333", Arrays.asList(1, 2, 3));
-        List<Integer> listFromSp = SPUtil.getListFromSp(mContext, "3333", Integer.class);
-        ExtendUtil.printList(listFromSp);
+        List<TestOo> listOo  = Arrays.asList(new TestOo("1111", 1111), new TestOo("2222", 2222), new TestOo("3333", 3333));
+        SPUtil.putListToSp("2222", listOo);
+//        SPUtil.putListToSp("3333", Arrays.asList(1, 2, 3));
+//        List<Integer> listFromSp = SPUtil.getListFromSp("3333", Integer.class);
+//        ExtendUtil.printList(listFromSp);
     }
-    
+
     public void testGetListFromSp() throws Exception {
-        List<TestOo> listFromSp = SPUtil.getListFromSp(mContext, "2222", TestOo.class);
+        List<TestOo> listFromSp = SPUtil.getListFromSp("2222", TestOo.class);
         ExtendUtil.printList(listFromSp);
     }
-    
+
     public void testSPUtils() throws Exception {
-        SPUtil.putBoolean(mContext, "aaaaaaaaa", true);
-//        SPUtil.putBoolean(mContext, "boolean", false);
-//        SPUtil.putFloat(mContext, "float", 5.20f);
-//        SPUtil.putInt(mContext, "int", 100);
-//        SPUtil.putLong(mContext, "long", 321651);
-//        SPUtil.putString(mContext, "string", "双方而上方的");
+        SPUtil.putBoolean("aaaaaaaaa", true);
+//        SPUtil.putBoolean("boolean", false);
+//        SPUtil.putFloat("float", 5.20f);
+//        SPUtil.putInt("int", 100);
+//        SPUtil.putLong("long", 321651);
+//        SPUtil.putString("string", "双方而上方的");
 //
 //        Set<String> texts = new HashSet<>();
 //        texts.add("董藩");
@@ -74,27 +77,27 @@ public class SPUtilTest extends BaseTestCase{
 //        intList.add(1);
 //        intList.add(2);
 //        intList.add(3);
-//        SPUtil.putIntList(mContext, "int_list", intList);
+//        SPUtil.putIntList("int_list", intList);
 //
 //        List<String> stringList = new ArrayList<>();
 //        stringList.add("我##fdf");
 //        stringList.add("是");
 //        stringList.add("谁");
-//        SPUtil.putStringList(mContext, "string_list", stringList);
+//        SPUtil.putStringList("string_list", stringList);
 
 
 //        SPUtil.clear(mContext);
 
-//        SPUtil.remove(mContext, "string_list");
-//        SPUtil.putInt(mContext, "int", 999);
+//        SPUtil.remove("string_list");
+//        SPUtil.putInt("int", 999);
 
-//        ExtendUtil.printList(SPUtil.getStringList(mContext, "string_list"));
-//        ExtendUtil.printList(SPUtil.getIntList(mContext, "int_list"));
-//        Logger.i(SPUtil.getInt(mContext, "sdjfo")+"");
+//        ExtendUtil.printList(SPUtil.getStringList("string_list"));
+//        ExtendUtil.printList(SPUtil.getIntList("int_list"));
+//        Logger.i(SPUtil.getInt("sdjfo")+"");
 
 
 //        SPUtil.getSP(mContext).edit().putBoolean("aaa", true).putString("bbb", "龙胆啊")
 //                .putLong("ccc", 7).putInt("ddd", 678).putFloat("eee", 20.222f).apply();
     }
-    
+
 }
--- applib/src/main/java/com/king/applib/util/SPUtil.java
+++ applib/src/main/java/com/king/applib/util/SPUtil.java
@@ -2,7 +2,6 @@ package com.king.applib.util;
 
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.support.annotation.NonNull;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -12,6 +11,7 @@ import java.util.List;
  * SharedPreferences 工具类.<br/>
  * ps:1.多进程中使用SharedPreference概率性取值出错
  * 2.同一时刻使用apply()方式提交,高概率出现保存不到sp的问题.可以put多次，最后一次apply()提交即可.
+ * 3.SharedPreferences不要保存太多的东西，耗内存.
  * Created by HuoGuangxu on 2016/10/19.
  */
 
@@ -20,92 +20,91 @@ public class SPUtil {
     public static final String SP_SEPARATOR = ";##;";
 
     private SPUtil() {
-        throw new UnsupportedOperationException("No instances!");
     }
 
-    public static SharedPreferences getSP(@NonNull Context context) {
-        return context.getSharedPreferences(SP_NAME, Context.MODE_PRIVATE);
+    public static SharedPreferences getSP() {
+        return ContextUtil.getAppContext().getSharedPreferences(SP_NAME, Context.MODE_PRIVATE);
     }
 
-    public static void putString(Context context, String key, String value) {
-        getSP(context).edit().putString(key, value).apply();
+    public static void putString(String key, String value) {
+        getSP().edit().putString(key, value).apply();
     }
 
-    public static String getString(Context context, String key) {
-        return getString(context, key, "");
+    public static String getString(String key) {
+        return getString(key, "");
     }
 
-    public static String getString(Context context, String key, @NonNull String defValue) {
-        return getSP(context).getString(key, defValue);
+    public static String getString(String key, String defValue) {
+        return getSP().getString(key, defValue);
     }
 
-    public static void putInt(Context context, String key, int value) {
-        getSP(context).edit().putInt(key, value).apply();
+    public static void putInt(String key, int value) {
+        getSP().edit().putInt(key, value).apply();
     }
 
-    public static int getInt(Context context, String key) {
-        return getInt(context, key, -1);
+    public static int getInt(String key) {
+        return getInt(key, -1);
     }
 
-    public static int getInt(Context context, String key, int defValue) {
-        return getSP(context).getInt(key, defValue);
+    public static int getInt(String key, int defValue) {
+        return getSP().getInt(key, defValue);
     }
 
-    public static void putFloat(Context context, String key, float value) {
-        getSP(context).edit().putFloat(key, value).apply();
+    public static void putFloat(String key, float value) {
+        getSP().edit().putFloat(key, value).apply();
     }
 
-    public static float getFloat(Context context, String key) {
-        return getFloat(context, key, -1.0f);
+    public static float getFloat(String key) {
+        return getFloat(key, -1.0f);
     }
 
-    public static float getFloat(Context context, String key, float defValue) {
-        return getSP(context).getFloat(key, defValue);
+    public static float getFloat(String key, float defValue) {
+        return getSP().getFloat(key, defValue);
     }
 
-    public static void putLong(Context context, String key, long value) {
-        getSP(context).edit().putLong(key, value).apply();
+    public static void putLong(String key, long value) {
+        getSP().edit().putLong(key, value).apply();
     }
 
-    public static long getLong(Context context, String key) {
-        return getLong(context, key, -1);
+    public static long getLong(String key) {
+        return getLong(key, -1);
     }
 
-    public static long getLong(Context context, String key, long defValue) {
-        return getSP(context).getLong(key, defValue);
+    public static long getLong(String key, long defValue) {
+        return getSP().getLong(key, defValue);
     }
 
-    public static void putBoolean(Context context, String key, boolean value) {
-        getSP(context).edit().putBoolean(key, value).apply();
+    public static void putBoolean(String key, boolean value) {
+        getSP().edit().putBoolean(key, value).apply();
     }
 
-    public static boolean getBoolean(Context context, String key) {
-        return getBoolean(context, key, false);
+    public static boolean getBoolean(String key) {
+        return getBoolean(key, false);
     }
 
-    public static boolean getBoolean(Context context, String key, boolean defValue) {
-        return getSP(context).getBoolean(key, defValue);
+    public static boolean getBoolean(String key, boolean defValue) {
+        return getSP().getBoolean(key, defValue);
     }
 
-    public static boolean contains(Context context, String key) {
-        return getSP(context).contains(key);
+    public static boolean contains(String key) {
+        return getSP().contains(key);
     }
 
-    public static void remove(Context context, String key) {
-        getSP(context).edit().remove(key).apply();
+    public static void remove(String key) {
+        getSP().edit().remove(key).apply();
     }
 
     /**
      * 清空sp
      */
-    public static void clear(Context context) {
-        getSP(context).edit().clear().apply();
+    public static void clear() {
+        getSP().edit().clear().apply();
     }
 
     /**
-     * 保存List<String>到sp.以{@link SPUtil#SP_SEPARATOR}分割,所以字符串中不要包含{@link SPUtil#SP_SEPARATOR},否则保存的和读取的不一致。
+     * 保存List<String>到sp.以{@link #SP_SEPARATOR}分割,所以字符串中不要包含{@link #SP_SEPARATOR},否则保存的和读取的不一致。
      */
-    public static void putStringList(@NonNull Context context, String key, List<String> list) {
+    public static void putStringList(String key, List<String> list) {
         if (ExtendUtil.isListNullOrEmpty(list)) {
             return;
         }
@@ -118,14 +117,14 @@ public class SPUtil {
             }
             sb.append(SP_SEPARATOR);
         }
-        getSP(context).edit().putString(key, sb.toString()).apply();
+        getSP().edit().putString(key, sb.toString()).apply();
     }
 
     /**
      * 获取指定key的List<String>
      */
-    public static List<String> getStringList(Context context, String key) {
-        String textList = getString(context, key);
+    public static List<String> getStringList(String key) {
+        String textList = getString(key);
         if (StringUtil.isNullOrEmpty(textList)) {
             return null;
         }
@@ -134,9 +133,9 @@ public class SPUtil {
     }
 
     /**
-     * 保存List<Integer>到sp.以{@link SPUtil#SP_SEPARATOR}分割,所以字符串中不要包含{@link SPUtil#SP_SEPARATOR},否则保存和读取的不一致。
+     * 保存List<Integer>到sp.以{@link #SP_SEPARATOR}分割,所以字符串中不要包含{@link #SP_SEPARATOR},否则保存的和读取的不一致。
      */
-    public static void putIntList(Context context, String key, List<Integer> list) {
+    public static void putIntList(String key, List<Integer> list) {
         if (ExtendUtil.isListNullOrEmpty(list)) {
             return;
         }
@@ -145,14 +144,14 @@ public class SPUtil {
         for (int value : list) {
             sb.append(value).append(SP_SEPARATOR);
         }
-        getSP(context).edit().putString(key, sb.toString()).apply();
+        getSP().edit().putString(key, sb.toString()).apply();
     }
 
     /**
-     * 获取指定key的List<Integer>.
+     * 获取指定key的List<Integer>
      */
-    public static List<Integer> getIntList(Context context, String key) {
-        String intList = getString(context, key);
+    public static List<Integer> getIntList(String key) {
+        String intList = getString(key);
         if (StringUtil.isNullOrEmpty(intList)) {
             return null;
         }
@@ -167,7 +166,7 @@ public class SPUtil {
     /***
      * 把List数据保存到SP
      */
-    public static <T> void putListToSp(Context context, String key, List<T> list) {
+    public static <T> void putListToSp(String key, List<T> list) {
         if (ExtendUtil.isListNullOrEmpty(list)) {
             return;
         }
@@ -175,18 +174,22 @@ public class SPUtil {
         for (T t : list) {
             sb.append(JsonUtil.encode(t)).append(SP_SEPARATOR);
         }
-        getSP(context).edit().putString(key, sb.toString()).apply();
+        getSP().edit().putString(key, sb.toString()).apply();
     }
 
     /***
      * 从SP中获取List类型的数据
      */
-    public static <T> List<T> getListFromSp(Context context, String key, Class<T> clazz) {
+    public static <T> List<T> getListFromSp(String key, Class<T> clazz) {
         List<T> list = new ArrayList<>();
-        String text = SPUtil.getString(context, key);
+        String text = SPUtil.getString(key);
         String[] classArray = text.trim().split(SP_SEPARATOR);
         for (String value : classArray) {
-            list.add(JsonUtil.decode(value, clazz));
+            T t = JsonUtil.decode(value, clazz);
+            if (t == null) {
+                continue;
+            }
+            list.add(t);
         }
         return list;
     }
