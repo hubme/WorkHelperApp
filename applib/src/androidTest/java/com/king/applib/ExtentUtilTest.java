a2727b305fb1781e6e05105ff1df3c9e1fa0e870
VanceKing <huogxu@gmail.com>
2017-06-12 19:37:30 +0800

    [test] 日历

--- applib/src/androidTest/java/com/king/applib/ExtentUtilTest.java
+++ applib/src/androidTest/java/com/king/applib/ExtentUtilTest.java
@@ -2,7 +2,11 @@ package com.king.applib;
 
 import com.king.applib.log.Logger;
 import com.king.applib.util.ContextUtil;
+import com.king.applib.util.DateTimeUtil;
 import com.king.applib.util.ExtendUtil;
+
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * @author huoguangxu
@@ -32,5 +36,22 @@ public class ExtentUtilTest extends BaseTestCase {
 
     public void testIsNotificationEnable() throws Exception {
         Logger.i(ExtendUtil.isNotificationEnable(mContext) ? "可用" : "不可用");
+    }
+
+    public void testCalendar() throws Exception{
+        Date date = new Date();//2016-06-09 16:46:30
+        String currentTime = DateTimeUtil.formatDate(date, DateTimeUtil.INTL_DATE_FORMAT);
+        Logger.i("currentTime: " + currentTime);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.set(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH), 0, 0, 0);
+        Logger.i("aaa: " + DateTimeUtil.formatDate(calendar.getTime(), DateTimeUtil.INTL_DATE_FORMAT));
+        
+        calendar.add(Calendar.MONTH, 1);
+        Date afterDate = calendar.getTime();
+
+        String afterDateStr = DateTimeUtil.formatDate(afterDate, DateTimeUtil.INTL_DATE_FORMAT);
+        Logger.i("afterDateStr: " + afterDateStr);
     }
 }
