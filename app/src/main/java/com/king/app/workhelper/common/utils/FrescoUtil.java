b1cc3946bddd13f05555bad9b7df0f7e072c7a38
VanceKing <huogxu@gmail.com>
2017-07-20 19:33:46 +0800

    [Utils] FrescoUtil

---
+++ app/src/main/java/com/king/app/workhelper/common/utils/FrescoUtil.java
@@ -0,0 +1,102 @@
+package com.king.app.workhelper.common.utils;
+
+import android.content.Context;
+import android.net.Uri;
+
+import com.facebook.binaryresource.BinaryResource;
+import com.facebook.binaryresource.FileBinaryResource;
+import com.facebook.cache.common.CacheKey;
+import com.facebook.common.executors.UiThreadImmediateExecutorService;
+import com.facebook.datasource.BaseDataSubscriber;
+import com.facebook.datasource.DataSource;
+import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.imagepipeline.cache.DefaultCacheKeyFactory;
+import com.facebook.imagepipeline.common.Priority;
+import com.facebook.imagepipeline.core.ImagePipeline;
+import com.facebook.imagepipeline.core.ImagePipelineFactory;
+import com.facebook.imagepipeline.request.ImageRequest;
+import com.facebook.imagepipeline.request.ImageRequestBuilder;
+import com.king.applib.log.Logger;
+import com.king.applib.util.ExtendUtil;
+import com.king.applib.util.StringUtil;
+
+import java.io.File;
+
+/**
+ * @author VanceKing
+ * @since 2017/7/20.
+ */
+
+public class FrescoUtil {
+    private FrescoUtil() {
+
+    }
+
+    public static void downloadImage(Context context, String url, ImageDownloadListener listener) {
+        if (context == null || StringUtil.isNullOrEmpty(url)) {
+            return;
+        }
+        ImageRequest imageRequest = ImageRequestBuilder.newBuilderWithSource(Uri.parse(url)).build();
+        ImagePipeline imagePipeline = Fresco.getImagePipeline();
+        DataSource<Void> dataSource = imagePipeline.prefetchToDiskCache(imageRequest, context, Priority.HIGH);
+        dataSource.subscribe(new BaseDataSubscriber<Void>() {
+            @Override protected void onNewResultImpl(DataSource<Void> dataSource) {
+                Logger.i("onSuccess.是否在主线程：" + ExtendUtil.isInMainThread());
+                if (listener != null) {
+                    listener.onSuccess(getFileFromDiskCache(context, url));
+                }
+                
+                //当CallerThreadExecutor.getInstance()时
+                /*getActivity().runOnUiThread(new Runnable() {
+                    @Override public void run() {
+                        mSimpleDraweeView.setImageURI(Uri.fromFile(fileFromDiskCache));
+                    }
+                });*/
+                
+            }
+
+            @Override
+            public void onProgressUpdate(DataSource<Void> dataSource) {
+                super.onProgressUpdate(dataSource);
+                Logger.i("progress: " + dataSource.getProgress());
+                if (listener != null) {
+                    listener.onProgress(dataSource.getProgress());
+                }
+            }
+
+            @Override
+            protected void onFailureImpl(DataSource<Void> dataSource) {
+                Logger.i("onFailure");
+                if (listener != null) {
+                    listener.onFail(url);
+                }
+            }
+        }, UiThreadImmediateExecutorService.getInstance());//CallerThreadExecutor.getInstance()
+    }
+
+    /**
+     * 从fresco的本地缓存拿到图片,注意文件的结束符并不是常见的.jpg,.png等，如果需要另存，可自行另存
+     */
+    public static File getFileFromDiskCache(Context context, String url) {
+        if (context == null || StringUtil.isNullOrEmpty(url)) {
+            return null;
+        }
+        CacheKey cacheKey = DefaultCacheKeyFactory.getInstance().getEncodedCacheKey(ImageRequest.fromUri(url), context);
+        if (ImagePipelineFactory.getInstance().getMainFileCache().hasKey(cacheKey)) {
+            BinaryResource resource = ImagePipelineFactory.getInstance().getMainFileCache().getResource(cacheKey);
+            return ((FileBinaryResource) resource).getFile();
+        } else if (ImagePipelineFactory.getInstance().getSmallImageFileCache().hasKey(cacheKey)) {
+            BinaryResource resource = ImagePipelineFactory.getInstance().getSmallImageFileCache().getResource(cacheKey);
+            return ((FileBinaryResource) resource).getFile();
+        }
+        return null;
+    }
+
+    public interface ImageDownloadListener {
+        void onSuccess(File file);
+
+        void onFail(String url);
+
+        void onProgress(float progress);
+    }
+}
--- app/src/main/java/com/king/app/workhelper/fragment/FrescoSampleFragment.java
+++ app/src/main/java/com/king/app/workhelper/fragment/FrescoSampleFragment.java
@@ -1,30 +1,43 @@
 package com.king.app.workhelper.fragment;
 
+import android.content.Context;
 import android.graphics.drawable.Animatable;
 import android.net.Uri;
 import android.os.Environment;
+import android.text.TextUtils;
+import android.webkit.URLUtil;
 
+import com.facebook.binaryresource.BinaryResource;
+import com.facebook.binaryresource.FileBinaryResource;
+import com.facebook.cache.common.CacheKey;
 import com.facebook.drawee.backends.pipeline.Fresco;
 import com.facebook.drawee.controller.AbstractDraweeController;
 import com.facebook.drawee.controller.BaseControllerListener;
 import com.facebook.drawee.view.SimpleDraweeView;
+import com.facebook.imagepipeline.cache.DefaultCacheKeyFactory;
+import com.facebook.imagepipeline.core.ImagePipelineFactory;
 import com.facebook.imagepipeline.image.ImageInfo;
+import com.facebook.imagepipeline.request.ImageRequest;
 import com.king.app.workhelper.R;
 import com.king.app.workhelper.common.AppBaseFragment;
 import com.king.applib.log.Logger;
 import com.king.applib.util.ExtendUtil;
 import com.king.applib.util.FileUtil;
 import com.king.applib.util.JsonUtil;
+import com.king.applib.util.StringUtil;
 import com.zhy.http.okhttp.OkHttpUtils;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.UUID;
 
 import javax.annotation.Nullable;
 
 import butterknife.BindView;
+import butterknife.OnClick;
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.Request;
@@ -38,8 +51,9 @@ import okhttp3.ResponseBody;
 
 public class FrescoSampleFragment extends AppBaseFragment {
     public static final String PIC_LOCAL_URL = "http://10.0.10.168:8080/appupdate/launch_background.jpg";
-        public static final String PIC_BAIDU_URL = "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1490190243757&di=3bd3e4eca13d247b62dad6e4dedaccc3&imgtype=0&src=http%3A%2F%2Ftupian.enterdesk.com%2F2016%2Fhxj%2F08%2F16%2F1602%2FChMkJlexsJmIIe8dAAghrgQhdMQAAUdNAJInfYACCHG699.jpg";
-    public static final String LOCAL_PIC = Environment.getExternalStorageDirectory().getPath() + "/000test/timg.jpg";
+    public static final String PIC_BAIDU_URL = "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1490190243757&di=3bd3e4eca13d247b62dad6e4dedaccc3&imgtype=0&src=http%3A%2F%2Ftupian.enterdesk.com%2F2016%2Fhxj%2F08%2F16%2F1602%2FChMkJlexsJmIIe8dAAghrgQhdMQAAUdNAJInfYACCHG699.jpg";
+    public static final String PIC_REMOTE_SNOW = "http://pic1.win4000.com/wallpaper/7/4fcec6cc47d34.jpg";
+    public static final String LOCAL_PIC = Environment.getExternalStorageDirectory().getPath() + "/000test/gjj_splash.jpg";
 
     @BindView(R.id.view_pic)
     SimpleDraweeView mSimpleDraweeView;
@@ -54,7 +68,7 @@ public class FrescoSampleFragment extends AppBaseFragment {
         super.initData();
 //        mSimpleDraweeView.setImageURI(Uri.fromFile(FileUtil.getFileByPath(LOCAL_PIC)));
 //        mSimpleDraweeView.setImageURI(Uri.parse("res://"+ ContextUtil.getAppContext().getPackageName()+"/"+R.mipmap.splash_screen));
-        mSimpleDraweeView.setImageURI(Uri.parse(PIC_LOCAL_URL));
+        mSimpleDraweeView.setImageURI(Uri.parse(PIC_BAIDU_URL));
 
         final AbstractDraweeController build = Fresco.newDraweeControllerBuilder()
                 .setControllerListener(new ImageDownloadListener())
@@ -133,12 +147,12 @@ public class FrescoSampleFragment extends AppBaseFragment {
 //        downloadLaunchImage();
 //        requestLaunchUrls();
     }
-    
-    private void requestLaunchUrls(){
+
+    private void requestLaunchUrls() {
         Request request = new Request.Builder().url("http://www.mocky.io/v2/58d36f5d100000b016b16690").build();
         OkHttpUtils.getInstance().getOkHttpClient().newCall(request).enqueue(new Callback() {
             @Override public void onFailure(Call call, IOException e) {
-                
+
             }
 
             @Override public void onResponse(Call call, Response response) throws IOException {
@@ -177,7 +191,7 @@ public class FrescoSampleFragment extends AppBaseFragment {
                 if (body == null) {
                     return;
                 }
-                Logger.i("文件大小："+body.contentLength()+"; 在主线程吗？ "+ ExtendUtil.isInMainThread());
+                Logger.i("文件大小：" + body.contentLength() + "; 在主线程吗？ " + ExtendUtil.isInMainThread());
                 boolean saveSuccess = FileUtil.saveStream(FileUtil.getFileByPath(Environment.getExternalStorageDirectory() + "/000test/test.jpg"), body.byteStream(), true);
                 Logger.i(saveSuccess ? "保存成功" : "保存失败");
             }
@@ -211,4 +225,83 @@ public class FrescoSampleFragment extends AppBaseFragment {
         }
     }
 
+    @OnClick(R.id.tv_download_image)
+    public void downloadImage() {
+        
+    }
+
+    /**
+     * 拷贝到某一个目录中,自动命名
+     */
+    public File copyCacheFileToDir(Context context, String url, File dir) {
+
+        if (dir == null) {
+            return null;
+        }
+        if (!dir.isDirectory()) {
+            throw new RuntimeException(dir + "is not a directory,you should call copyCacheFile(String url,File path)");
+        }
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+        String fileName = URLUtil.guessFileName(url, "", "");//android SDK 提供的方法.
+        // 注意不能直接采用file的getName拿到文件名,因为缓存文件是用cacheKey命名的
+        if (TextUtils.isEmpty(fileName)) {
+            fileName = UUID.randomUUID().toString();
+        }
+        File newFile = new File(dir, fileName);
+
+        boolean isSuccess = copyCacheFile(context, url, newFile);
+        if (isSuccess) {
+            return newFile;
+        } else {
+            return null;
+        }
+
+    }
+
+    /**
+     * 拷贝到某一个文件,已指定文件名
+     *
+     * @param url  图片的完整url
+     * @param path 目标文件路径
+     */
+    public boolean copyCacheFile(Context context, String url, File path) {
+        if (path == null) {
+            return false;
+        }
+        File file = getFileFromDiskCache(context, url);
+        if (file == null) {
+            return false;
+        }
+
+        if (path.isDirectory()) {
+            throw new RuntimeException(path + "is a directory,you should call copyCacheFileToDir(String url,File dir)");
+        }
+        boolean isSuccess = file.renameTo(path);
+
+        return isSuccess;
+    }
+
+    /**
+     * 从fresco的本地缓存拿到图片,注意文件的结束符并不是常见的.jpg,.png等，如果需要另存，可自行另存
+     */
+    public static File getFileFromDiskCache(Context context, String url) {
+        if (context == null || StringUtil.isNullOrEmpty(url)) {
+            return null;
+        }
+        CacheKey cacheKey = DefaultCacheKeyFactory.getInstance().getEncodedCacheKey(ImageRequest.fromUri(url), context);
+        if (ImagePipelineFactory.getInstance().getMainFileCache().hasKey(cacheKey)) {
+            BinaryResource resource = ImagePipelineFactory.getInstance().getMainFileCache().getResource(cacheKey);
+            return ((FileBinaryResource) resource).getFile();
+        } else if (ImagePipelineFactory.getInstance().getSmallImageFileCache().hasKey(cacheKey)) {
+            BinaryResource resource = ImagePipelineFactory.getInstance().getSmallImageFileCache().getResource(cacheKey);
+            return ((FileBinaryResource) resource).getFile();
+        }
+        return null;
+    }
+
+    @OnClick(R.id.tv_update_image)
+    public void onImageUpdateClick() {
+    }
 }
