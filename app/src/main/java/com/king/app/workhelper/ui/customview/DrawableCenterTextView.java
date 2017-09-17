387030a1c484b43fbacc66bed709ed19b327b00c
VanceKing <huogxu@gmail.com>
2017-09-17 19:15:49 +0800

    [feature] Drawable Center TextView

--- app/src/main/java/com/king/app/workhelper/ui/customview/DrawableCenterTextView.java
+++ app/src/main/java/com/king/app/workhelper/ui/customview/DrawableCenterTextView.java
@@ -5,14 +5,16 @@ package com.king.app.workhelper.ui.customview;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.text.TextPaint;
 import android.util.AttributeSet;
-import android.widget.TextView;
+import android.util.Log;
 
-public class DrawableCenterTextView extends TextView {
+public class DrawableCenterTextView extends android.support.v7.widget.AppCompatTextView {
 
-    public DrawableCenterTextView(Context context, AttributeSet attrs,
-                                  int defStyle) {
+    public DrawableCenterTextView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
     }
 
@@ -27,17 +29,51 @@ public class DrawableCenterTextView extends TextView {
     @Override
     protected void onDraw(Canvas canvas) {
         Drawable[] drawables = getCompoundDrawables();
-        if (drawables != null) {
-            Drawable drawableLeft = drawables[0];
-            if (drawableLeft != null) {
-                float textWidth = getPaint().measureText(getText().toString());
-                int drawablePadding = getCompoundDrawablePadding();
-                int drawableWidth = 0;
-                drawableWidth = drawableLeft.getIntrinsicWidth();
-                float bodyWidth = textWidth + drawableWidth + drawablePadding;
-                canvas.translate((getWidth() - bodyWidth) / 2, 0);
-            }
+        Drawable drawableLeft = drawables[0];
+        Drawable drawableTop = drawables[1];
+        Drawable drawableRight = drawables[2];
+        Drawable drawableBottom = drawables[3];
+
+        int drawablePadding = getCompoundDrawablePadding();
+
+        int drawableWidth = (drawableLeft == null ? 0 : drawableLeft.getIntrinsicWidth()) + (drawableRight == null ? 0 : drawableRight.getIntrinsicWidth());
+        int drawableHeight = (drawableTop == null ? 0 : drawableTop.getIntrinsicWidth()) + (drawableBottom == null ? 0 : drawableBottom.getIntrinsicWidth());
+
+        final TextPaint textPaint = getPaint();
+        float textWidth = textPaint.measureText(getText().toString());
+
+        Paint.FontMetrics fontMetrics = textPaint.getFontMetrics();
+        float textHeight = fontMetrics.descent - fontMetrics.ascent;
+
+        float contentHeight = textHeight + drawableHeight + drawablePadding;
+        
+        float dy = (contentHeight - getHeight())/2;
+        Log.i("aaa", "contentHeight: " + contentHeight + ";getHeight(): " + getHeight() + ";dy: " + dy);
+//        canvas.translate(0, -dy);
+        if (drawableTop != null) {
+            canvas.drawBitmap(((BitmapDrawable)drawableTop).getBitmap(), 20, 20, null);
         }
         super.onDraw(canvas);
+        
+    }
+
+    private Canvas getTopCanvas(Canvas canvas) {
+        Drawable[] drawables = getCompoundDrawables();
+        Drawable drawable = drawables[1];// 上面的drawable  
+        if(drawable == null){
+            drawable = drawables[3];// 下面的drawable  
+        }
+
+        float textSize = getPaint().getTextSize();
+        int drawHeight = drawable.getIntrinsicHeight();
+        int drawPadding = getCompoundDrawablePadding();
+        float contentHeight = textSize + drawHeight + drawPadding;
+        int topPadding = (int) (getHeight() - contentHeight);
+        setPadding(0, topPadding , 0, 0);
+        float dy = (contentHeight - getHeight())/2;
+        canvas.translate(0, dy);
+        Log.i("DrawableTopButton", "setPadding(0,"+topPadding+",0,0");
+        Log.i("DrawableTopButton", "translate(0,"+dy+")");
+        return canvas;
     }
 }
---
+++ app/src/main/java/com/king/app/workhelper/ui/customview/DrawableVerticalTextView.java
@@ -0,0 +1,52 @@
+package com.king.app.workhelper.ui.customview;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.util.Log;
+
+/**
+ * @author huoguangxu
+ * @since 2017/9/17.
+ */
+
+public class DrawableVerticalTextView extends android.support.v7.widget.AppCompatTextView {
+    public DrawableVerticalTextView(Context context) {
+        super(context);
+    }
+
+    public DrawableVerticalTextView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public DrawableVerticalTextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override protected void onDraw(Canvas canvas) {
+        canvas = getTopCanvas(canvas);
+        super.onDraw(canvas);
+    }
+
+    private Canvas getTopCanvas(Canvas canvas) {
+        Drawable[] drawables = getCompoundDrawables();
+        Drawable drawable = drawables[1];// 上面的drawable  
+        if (drawable == null) {
+            drawable = drawables[3];// 下面的drawable  
+        }
+
+        float textSize = getPaint().getTextSize();
+        int drawHeight = drawable.getIntrinsicHeight();
+        int drawPadding = getCompoundDrawablePadding();
+        float contentHeight = textSize + drawHeight + drawPadding;
+        int topPadding = (int) (getHeight() - contentHeight);
+        setPadding(0, topPadding, 0, 0);
+        float dy = (contentHeight - getHeight()) / 2;
+        canvas.translate(0, dy);
+        Log.i("aaa", "setPadding(0," + topPadding + ",0,0");
+        Log.i("aaa", "translate(0," + dy + ")");
+        return canvas;
+    }
+}
