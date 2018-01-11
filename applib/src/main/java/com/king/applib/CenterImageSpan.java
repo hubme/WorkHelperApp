9f42834154215759cb19615378a9a384e133c3cb
VanceKing <huogxu@gmail.com>
2018-01-11 11:00:07 +0800

    [feature] CenterImageSpan

---
+++ applib/src/main/java/com/king/applib/CenterImageSpan.java
@@ -0,0 +1,37 @@
+package com.king.applib;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.NonNull;
+import android.text.style.ImageSpan;
+
+/**
+ * 解决图片垂直居中的问题
+ *
+ * @author VanceKing
+ */
+public class CenterImageSpan extends ImageSpan {
+
+    public CenterImageSpan(Context context, final int drawableRes) {
+        super(context, drawableRes);
+    }
+
+    public CenterImageSpan(Drawable d) {
+        super(d);
+    }
+
+    @Override
+    public void draw(@NonNull Canvas canvas, CharSequence text, int start, int end, float x,
+                     int top, int y, int bottom, @NonNull Paint paint) {
+        Drawable b = getDrawable();
+        Paint.FontMetricsInt fm = paint.getFontMetricsInt();
+        int transY = (y + fm.descent + y + fm.ascent) / 2 - b.getBounds().bottom / 2;
+
+        canvas.save();
+        canvas.translate(x, transY);
+        b.draw(canvas);
+        canvas.restore();
+    }
+}
