459c328256bd3c5d8472107ef68af2f88fa1c954
VanceKing <huogxu@gmail.com>
2018-01-21 23:02:45 +0800

    [refactor] 去掉tbs

--- app/src/main/java/com/king/app/workhelper/app/WorkHelperApp.java
+++ app/src/main/java/com/king/app/workhelper/app/WorkHelperApp.java
@@ -26,7 +26,6 @@ import com.king.applib.util.AppUtil;
 import com.king.applib.util.ContextUtil;
 import com.king.applib.util.FileUtil;
 import com.squareup.leakcanary.RefWatcher;
-import com.tencent.smtt.sdk.QbSdk;
 import com.zhy.http.okhttp.OkHttpUtils;
 
 import java.util.concurrent.TimeUnit;
@@ -141,18 +140,18 @@ public class WorkHelperApp extends BaseApplication {
         return application.mRefWatcher;
     }
 
-    private void initX5() {
+    /*private void initX5() {
         QbSdk.initX5Environment(getApplicationContext(), new QbSdk.PreInitCallback() {
             @Override public void onCoreInitFinished() {
-//                Logger.i("onCoreInitFinished");
+                Logger.i("onCoreInitFinished");
             }
 
             @Override public void onViewInitFinished(boolean success) {
                 //x5內核初始化完成的回调，为true表示x5内核加载成功，否则表示x5内核加载失败，会自动切换到系统内核。
-//                Logger.i("x5 init success");
+                Logger.i("x5 init success");
             }
         });
-    }
+    }*/
 
     @Override
     public void onTerminate() {
