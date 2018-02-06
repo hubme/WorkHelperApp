41508c5d92dd7b32b071a357c3c4225b2fe6fa83
VanceKing <huogxu@gmail.com>
2018-02-06 23:06:56 +0800

    [View] Canvas 相关操作

--- app/src/main/java/com/king/app/workhelper/ui/customview/CanvasRotateTestView.java
+++ app/src/main/java/com/king/app/workhelper/ui/customview/CanvasRotateTestView.java
@@ -2,6 +2,8 @@ package com.king.app.workhelper.ui.customview;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Rect;
 import android.support.annotation.Nullable;
 import android.text.TextPaint;
 import android.util.AttributeSet;
@@ -17,6 +19,7 @@ import com.king.applib.base.customview.BaseView;
 
 public class CanvasRotateTestView extends BaseView {
     private static final String TEXT = "哈哈哈";
+    private Rect rect = new Rect(10,10,200,100);
 
     public CanvasRotateTestView(Context context) {
         super(context);
@@ -37,8 +40,57 @@ public class CanvasRotateTestView extends BaseView {
 
     @Override protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        canvas.drawColor(Color.BLUE);
+        canvas.drawRect(new Rect(0, 0, 400, 400), mPaint);
 
-        testRotate(canvas);
+        // 保存画布状态  
+        canvas.save();
+        canvas.scale(0.5f, 0.5f);
+        mPaint.setColor(Color.YELLOW);
+        canvas.drawRect(new Rect(0, 0, 400, 400), mPaint);
+        // 画布状态回滚  
+        canvas.restore();
+
+        canvas.scale(0.5f, 0.5f, 200, 200);
+        mPaint.setColor(Color.BLACK);
+        canvas.drawRect(new Rect(0, 0, 400, 400), mPaint);
+    }
+
+    private void testClip(Canvas canvas) {
+        canvas.drawColor(Color.GREEN);
+        canvas.clipRect(rect);
+        canvas.drawColor(Color.RED);
+    }
+
+    private void testSkew(Canvas canvas) {
+        mPaint.setStrokeWidth(10);
+        canvas.drawRect(rect, mPaint);
+        canvas.skew(1.732f,0f);//X轴倾斜60度，Y轴不变  
+
+        mPaint.setColor(Color.GREEN);
+        canvas.drawRect(rect, mPaint);
+    }
+
+    private void testScale(Canvas canvas) {
+        canvas.drawRect(getViewHalfWidth()-200, 0, getViewHalfWidth() + 200, 200, mTextPaint);
+
+        canvas.scale(1.2f, 0.5f, getViewHalfWidth(), getViewHalfHeight());
+
+        mTextPaint.setColor(Color.BLACK);
+        canvas.drawRect(getViewHalfWidth()-200, 0, getViewHalfWidth() + 200, 200, mTextPaint);
+
+    }
+
+    private void testRotate2(Canvas canvas) {
+        int rectWidth = 200;
+        int rectHeight = 400;
+
+        canvas.drawRect(getViewHalfWidth() - rectWidth / 2, getViewHalfHeight() - rectHeight / 2, getViewHalfWidth() + rectWidth / 2, getViewHalfHeight() + rectHeight / 2, mTextPaint);
+        
+        canvas.rotate(45, getViewHalfWidth(), getViewHalfHeight());
+
+        mTextPaint.setColor(Color.BLACK);
+        canvas.drawRect(getViewHalfWidth() - rectWidth / 2, getViewHalfHeight() - rectHeight / 2, getViewHalfWidth() + rectWidth / 2, getViewHalfHeight() + rectHeight / 2, mTextPaint);
 
     }
 
@@ -49,8 +101,11 @@ public class CanvasRotateTestView extends BaseView {
         int y1 = 0;
         canvas.drawRect(x1, y1, x1 + rectWidth, y1 + rectHeight, mPaint);
 
-        canvas.translate(x1, y1);
-        canvas.rotate(30);
+        canvas.translate(x1, 0);
+        canvas.rotate(90);
+
+        x1 = 0;
+        y1 = 0;
         canvas.drawRect(x1, y1, x1 + rectWidth, y1 + rectHeight, mPaint);
 
     }
