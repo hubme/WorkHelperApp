27cc13e0fe5e215b5d265e2e3429d97c014233ec
VanceKing <huogxu@gmail.com>
2018-02-08 10:10:03 +0800

    [test] android.text.format.DateUtils 测试

--- applib/src/androidTest/java/com/king/applib/DateTimeUtilTest.java
+++ applib/src/androidTest/java/com/king/applib/DateTimeUtilTest.java
@@ -1,6 +1,8 @@
 package com.king.applib;
 
 
+import android.text.format.DateUtils;
+
 import com.king.applib.log.Logger;
 import com.king.applib.util.DateTimeUtil;
 
@@ -36,7 +38,7 @@ public class DateTimeUtilTest extends BaseTestCase {
     }
 
     public void testBetweenDurations() throws Exception {
-        Logger.i("全部比较: " + DateTimeUtil.betweenDurations(DateTimeUtil.INTL_DATE_FORMAT, "yyyy-MM-dd HH:mm:ss", "2017-10-23 13:01:01", "2017-10-23 12:00:00"));//3661000 mils
+        Logger.i("全部比较: " + DateTimeUtil.betweenDurations(DateTimeUtil.INTL_DATE_FORMAT, "yyyy-MM-dd HH:mm:ss", "2017-10-23 13:01:01", "2017-10-23 12:00:00"));//timeMillis000 mils
         Logger.i("不比较秒: " + DateTimeUtil.betweenDurations(DateTimeUtil.INTL_DATE_FORMAT, "yyyy-MM-dd HH:mm", "2017-10-23 12:00:00", "2017-10-23 13:01:01"));//3660000 mils
         Logger.i("不比较分秒: " + DateTimeUtil.betweenDurations(DateTimeUtil.INTL_DATE_FORMAT, "yyyy-MM-dd HH", "2017-10-23 12:00:00", "2017-10-23 13:01:01"));//3600000
         Logger.i("只比较小时: " + DateTimeUtil.betweenDurations(DateTimeUtil.INTL_DATE_FORMAT, "HH", "2017-10-23 12:00:00", "2017-10-23 13:01:01"));//3600000
@@ -53,7 +55,7 @@ public class DateTimeUtilTest extends BaseTestCase {
         Logger.i("afterDate: " + DateTimeUtil.formatDate(date, DateTimeUtil.INTL_DATE_FORMAT));
     }
 
-    public void testCalendar() throws Exception{
+    public void testCalendar() throws Exception {
         Date date = new Date();//2016-06-09 16:46:30
         String currentTime = DateTimeUtil.formatDate(date, DateTimeUtil.INTL_DATE_FORMAT);
         Logger.i("currentTime: " + currentTime);
@@ -68,5 +70,31 @@ public class DateTimeUtilTest extends BaseTestCase {
 
         String afterDateStr = DateTimeUtil.formatDate(afterDate, DateTimeUtil.INTL_DATE_FORMAT);
         Logger.i("afterDateStr: " + afterDateStr);
+    }
+
+    /*
+    * FORMAT_SHOW_DATE：3月3日
+	* FORMAT_SHOW_TIME：10:37
+	* FORMAT_SHOW_WEEKDAY：星期五
+	* FORMAT_SHOW_YEAR：2017年3月3日
+	* FORMAT_NUMERIC_DATE：3/3
+	* FORMAT_NO_MONTH_DAY：三月
+     */
+    public void testDateUtils() throws Exception {
+        Calendar calendar = Calendar.getInstance();
+        //month 会+1
+        calendar.set(2018, 2, 8, 10, 4, 8);//2018-03-08 10:04:08
+        Date date = calendar.getTime();
+        long timeMillis = date.getTime();
+        Logger.i("FORMAT_SHOW_DATE: " + DateUtils.formatDateTime(getContext(), timeMillis, DateUtils.FORMAT_SHOW_DATE) +
+                "\nFORMAT_SHOW_TIME: " + DateUtils.formatDateTime(getContext(), timeMillis, DateUtils.FORMAT_SHOW_TIME) +
+                "\nFORMAT_SHOW_WEEKDAY: " + DateUtils.formatDateTime(getContext(), timeMillis, DateUtils.FORMAT_SHOW_WEEKDAY) +
+                "\nFORMAT_SHOW_YEAR: " + DateUtils.formatDateTime(getContext(), timeMillis, DateUtils.FORMAT_SHOW_YEAR) +
+                "\nFORMAT_NUMERIC_DATE: " + DateUtils.formatDateTime(getContext(), timeMillis, DateUtils.FORMAT_NUMERIC_DATE) +
+                "\nFORMAT_NO_MONTH_DAY: " + DateUtils.formatDateTime(getContext(), timeMillis, DateUtils.FORMAT_NO_MONTH_DAY)
+
+        );
+
+
     }
 }
