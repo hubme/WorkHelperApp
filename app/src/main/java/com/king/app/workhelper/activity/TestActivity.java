881601b4fa0c19bd0dfa0056f4ceaf00c25c3764
VanceKing <huogxu@gmail.com>
2018-02-22 11:38:01 +0800

    [feature] TransitionDrawable usage

--- app/src/main/java/com/king/app/workhelper/activity/TestActivity.java
+++ app/src/main/java/com/king/app/workhelper/activity/TestActivity.java
@@ -1,10 +1,15 @@
 package com.king.app.workhelper.activity;
 
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.TransitionDrawable;
+import android.support.v4.content.ContextCompat;
 import android.widget.TextView;
 
 import com.king.app.workhelper.R;
 import com.king.app.workhelper.common.AppBaseActivity;
+import com.king.applib.util.ViewUtil;
 
+import butterknife.BindView;
 import butterknife.OnClick;
 
 /**
@@ -13,6 +18,10 @@ import butterknife.OnClick;
  */
 
 public class TestActivity extends AppBaseActivity {
+    @BindView(R.id.tv_transition_drawable_sample1) TextView mDrawableTv1;
+    @BindView(R.id.tv_transition_drawable_sample2) TextView mDrawableTv2;
+    private TransitionDrawable transitionDrawable1;
+    private TransitionDrawable transitionDrawable2;
 
     @Override protected int getContentLayout() {
         return R.layout.activity_test;
@@ -21,13 +30,23 @@ public class TestActivity extends AppBaseActivity {
     @Override protected void initContentView() {
         super.initContentView();
 
+        Drawable[] drawables = {ContextCompat.getDrawable(this, R.drawable.icon_3),
+                ContextCompat.getDrawable(this, R.drawable.icon_4)};
+        transitionDrawable1 = new TransitionDrawable(drawables);
+        transitionDrawable1.setCrossFadeEnabled(true);
+        ViewUtil.setViewBackground(mDrawableTv1, transitionDrawable1);
+
+        transitionDrawable2 = (TransitionDrawable) mDrawableTv2.getBackground();
+        transitionDrawable2.setCrossFadeEnabled(false);
+
     }
 
     @OnClick(R.id.tv_open_qq)
     public void openQQ() {
-        
+        transitionDrawable1.startTransition(2000);
+        transitionDrawable2.startTransition(2000);
     }
-    
+
     @OnClick(R.id.tv_open_email)
     public void openEmail(TextView textView) {
 
