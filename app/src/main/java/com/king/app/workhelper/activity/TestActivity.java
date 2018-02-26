7ea4851efc55617d42bcf242a375a1469c1a204b
VanceKing <huogxu@gmail.com>
2018-02-26 15:01:22 +0800

    [feature] 自定义文字颜色渐变TextView

--- app/src/main/java/com/king/app/workhelper/activity/TestActivity.java
+++ app/src/main/java/com/king/app/workhelper/activity/TestActivity.java
@@ -1,16 +1,10 @@
 package com.king.app.workhelper.activity;
 
-import android.graphics.drawable.Drawable;
-import android.graphics.drawable.TransitionDrawable;
-import android.support.v4.content.ContextCompat;
-import android.widget.TextView;
-
 import com.king.app.workhelper.R;
 import com.king.app.workhelper.common.AppBaseActivity;
-import com.king.applib.util.ViewUtil;
+import com.king.applib.ui.customview.GradientTextView;
 
 import butterknife.BindView;
-import butterknife.OnClick;
 
 /**
  * @author VanceKing
@@ -18,10 +12,7 @@ import butterknife.OnClick;
  */
 
 public class TestActivity extends AppBaseActivity {
-    @BindView(R.id.tv_transition_drawable_sample1) TextView mDrawableTv1;
-    @BindView(R.id.tv_transition_drawable_sample2) TextView mDrawableTv2;
-    private TransitionDrawable transitionDrawable1;
-    private TransitionDrawable transitionDrawable2;
+    @BindView(R.id.tv_open_qq) GradientTextView mTextView;
 
     @Override protected int getContentLayout() {
         return R.layout.activity_test;
@@ -30,26 +21,7 @@ public class TestActivity extends AppBaseActivity {
     @Override protected void initContentView() {
         super.initContentView();
 
-        Drawable[] drawables = {ContextCompat.getDrawable(this, R.drawable.icon_3),
-                ContextCompat.getDrawable(this, R.drawable.icon_4)};
-        transitionDrawable1 = new TransitionDrawable(drawables);
-        transitionDrawable1.setCrossFadeEnabled(true);
-        ViewUtil.setViewBackground(mDrawableTv1, transitionDrawable1);
+    
 
-        transitionDrawable2 = (TransitionDrawable) mDrawableTv2.getBackground();
-        transitionDrawable2.setCrossFadeEnabled(false);
-
-    }
-
-    @OnClick(R.id.tv_open_qq)
-    public void openQQ() {
-        transitionDrawable1.startTransition(2000);
-        transitionDrawable2.startTransition(2000);
-    }
-
-    @OnClick(R.id.tv_open_email)
-    public void openEmail(TextView textView) {
-
-        textView.animate().alpha(0.2f).setDuration(800).start();
     }
 }
---
+++ applib/src/main/java/com/king/applib/ui/customview/GradientTextView.java
@@ -0,0 +1,84 @@
+package com.king.applib.ui.customview;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Paint;
+import android.graphics.Shader;
+import android.support.annotation.Nullable;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.Gravity;
+
+import com.king.applib.R;
+
+/**
+ * 文字颜色渐变。
+ *
+ * @author VanceKing
+ * @since 2018/2/26.
+ */
+
+public class GradientTextView extends android.support.v7.widget.AppCompatTextView {
+    private int mStartColor = Color.BLACK;
+    private int mEndColor = Color.BLACK;
+    //默认不渐变
+    private int mGradientType = -1;
+
+    public GradientTextView(Context context) {
+        this(context, null);
+    }
+
+    public GradientTextView(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public GradientTextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+        TypedArray typedArray = null;
+        try {
+            typedArray = context.obtainStyledAttributes(attrs, R.styleable.GradientTextView);
+            mStartColor = typedArray.getColor(R.styleable.GradientTextView_gtv_startColor, Color.BLACK);
+            mEndColor = typedArray.getColor(R.styleable.GradientTextView_gtv_endColor, Color.BLACK);
+            mGradientType = typedArray.getInt(R.styleable.GradientTextView_gtv_gradientType, -1);
+        } finally {
+            if (typedArray != null) {
+                typedArray.recycle();
+            }
+        }
+    }
+
+    @SuppressLint("DrawAllocation") @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+        if (!changed || TextUtils.isEmpty(getText().toString().trim()) || mGradientType == -1) {
+            return;
+        }
+
+        if (mGradientType == 0) {
+            float textWidth = getPaint().measureText(getText().toString());
+            int gravity = getGravity() & Gravity.HORIZONTAL_GRAVITY_MASK;
+            if (gravity == Gravity.LEFT) {
+                Log.i("aaa", "Gravity.LEFT");
+                getPaint().setShader(new LinearGradient(0, 0, textWidth, 0, Color.RED, Color.GREEN, Shader.TileMode.CLAMP));
+            } else if (gravity == Gravity.CENTER_HORIZONTAL) {
+                Log.i("aaa", "Gravity.CENTER");
+                getPaint().setShader(new LinearGradient((getMeasuredWidth() - textWidth) / 2, 0,
+                        (getMeasuredWidth() + textWidth) / 2, 0, Color.RED, Color.GREEN, Shader.TileMode.CLAMP));
+            } else if (gravity == Gravity.RIGHT) {
+                Log.i("aaa", "Gravity.END");
+                getPaint().setShader(new LinearGradient(getMeasuredWidth() - textWidth, 0,
+                        getMeasuredWidth(), 0, Color.RED, Color.GREEN, Shader.TileMode.CLAMP));
+            }
+
+        } else if (mGradientType == 1) {
+            Paint.FontMetrics fontMetrics = getPaint().getFontMetrics();
+            getPaint().setShader(new LinearGradient(0, -fontMetrics.ascent, 0, fontMetrics.descent, mEndColor,
+                    mStartColor, Shader.TileMode.CLAMP));
+        }
+    }
+}
