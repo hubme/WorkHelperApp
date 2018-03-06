8f76729212dbe5b41c03b859303a8704f6f3d6de
VanceKing <huogxu@gmail.com>
2018-03-06 23:09:53 +0800

    [test] Queue使用

--- app/src/androidTest/java/com/king/app/workhelper/DataStructureTest.java
+++ app/src/androidTest/java/com/king/app/workhelper/DataStructureTest.java
@@ -1,10 +1,17 @@
 package com.king.app.workhelper;
 
+import android.support.annotation.NonNull;
 import android.support.v4.util.Pools;
+import android.util.Log;
 
 import com.king.applib.log.Logger;
 
 import org.junit.Test;
+
+import java.util.LinkedList;
+import java.util.concurrent.DelayQueue;
+import java.util.concurrent.Delayed;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author VanceKing
@@ -24,7 +31,7 @@ public class DataStructureTest extends BaseTestCase {
         }
     }
 
-    private static class PersonPool{
+    private static class PersonPool {
         private Pools.SimplePool<Person> personSimplePool = new Pools.SimplePool<>(3);
 
         public Person obtain() {
@@ -43,8 +50,60 @@ public class DataStructureTest extends BaseTestCase {
 
     }
 
+    @Test
+    public void testLinkedList() {
+        LinkedList<Person> persons = new LinkedList<>();
+        persons.add(new Person("aaa"));
+        persons.add(new Person("bbb"));
+        persons.add(new Person("ccc"));
 
-    private static class Person {
+        Person polled = persons.poll();
+        Logger.i(polled.toString());
+        Logger.i(String.valueOf(persons.size()));
+        persons.addLast(polled);
+        printList(persons);
+    }
+
+    public void testDelayQueue() {
+        DelayQueue<Person> persons = new DelayQueue<>();
+        persons.add(new Person("aaa", 1000));
+        persons.add(new Person("bbb", 2000));
+        persons.add(new Person("ccc", 3000));
+
+        for (Person person : persons) {
+            Log.i("aaa", person.toString());
+        }
+    }
+
+    private static class Person implements Delayed {
         private String name;
+        private long expire;//到期时间
+
+        public Person() {
+
+        }
+
+        public Person(String name) {
+            this.name = name;
+        }
+
+        public Person(String name, long delay) {
+            this.name = name;
+            expire = System.currentTimeMillis() + delay;//到期时间 = 当前时间+延迟时间
+        }
+
+        @Override public String toString() {
+            return "Person{" +
+                    "name='" + name + '\'' +
+                    '}';
+        }
+
+        @Override public long getDelay(@NonNull TimeUnit unit) {
+            return unit.convert(this.expire - System.currentTimeMillis(), TimeUnit.MILLISECONDS);
+        }
+
+        @Override public int compareTo(@NonNull Delayed o) {
+            return (int) (this.getDelay(TimeUnit.MILLISECONDS) - o.getDelay(TimeUnit.MILLISECONDS));
+        }
     }
 }
