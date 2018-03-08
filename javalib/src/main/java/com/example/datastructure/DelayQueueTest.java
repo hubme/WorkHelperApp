bdce463d7c506382e1ebad0c0a1d9672685224d8
VanceKing <huogxu@gmail.com>
2018-03-08 23:26:21 +0800

    [test] DelayQueue 使用

---
+++ javalib/src/main/java/com/example/datastructure/DelayQueueTest.java
@@ -0,0 +1,93 @@
+package com.example.datastructure;
+
+import java.util.concurrent.DelayQueue;
+import java.util.concurrent.Delayed;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author VanceKing
+ * @since 2018/3/6.
+ */
+
+public class DelayQueueTest {
+    public static void main(String[] args) {
+        // 创建延时队列  
+        DelayQueue<Message> queue = new DelayQueue<>();
+        Message m1 = new Message(1, "world", 1000);
+        Message m2 = new Message(2, "hello", 3000);
+        queue.add(m1);
+        queue.add(m2);
+        new Thread(new Consumer(queue)).start();
+    }
+
+    public static class Consumer implements Runnable {
+
+        // 延时队列  
+        private DelayQueue<Message> queue;
+
+        public Consumer(DelayQueue<Message> queue) {
+            this.queue = queue;
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    Message take = queue.take();
+                    System.out.println("custom is: " + take.getId() + ":" + take.getBody());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static class Message implements Delayed {
+
+        private int id;
+        private String body;  //消息内容  
+        private long excuteTime;//执行时间      
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getBody() {
+            return body;
+        }
+
+        public void setBody(String body) {
+            this.body = body;
+        }
+
+        public long getExcuteTime() {
+            return excuteTime;
+        }
+
+        public void setExcuteTime(long excuteTime) {
+            this.excuteTime = excuteTime;
+        }
+
+        public Message(int id, String body, long delayTime) {
+            this.id = id;
+            this.body = body;
+            this.excuteTime = TimeUnit.NANOSECONDS.convert(delayTime, TimeUnit.MILLISECONDS) + System.nanoTime();
+        }
+
+        @Override
+        public int compareTo(Delayed delayed) {
+            Message msg = (Message) delayed;
+            return this.id > msg.id ? 1 : (this.id < msg.id ? -1 : 0);
+        }
+
+        @Override
+        public long getDelay(TimeUnit unit) {
+            return unit.convert(this.excuteTime - System.nanoTime(), TimeUnit.NANOSECONDS);
+        }
+
+    }
+}
