7e48526a2acf8a38bada7dbfc80c29761b84ea01
VanceKing <huogxu@gmail.com>
2018-03-19 19:06:41 +0800

    [feature] 无横向滑动冲突的SwipeRefreshLayout

---
+++ app/src/main/java/com/king/app/workhelper/ui/customview/SimpleSwipeRefreshLayout.java
@@ -0,0 +1,66 @@
+package com.king.app.workhelper.ui.customview;
+
+import android.content.Context;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.ViewConfiguration;
+
+/**
+ * 解决 ViewPager 横向滑动冲突。
+ * http://blog.csdn.net/u010386612/article/details/50548977
+ *
+ * @author VanceKing
+ * @since 2018/3/19.
+ */
+
+public class SimpleSwipeRefreshLayout extends SwipeRefreshLayout {
+    private float mStartX;
+    private float mStartY;
+    // 记录ViewPager是否拖拽的标记
+    private boolean mIsDrag;
+    private final int mTouchSlop;
+
+    public SimpleSwipeRefreshLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mTouchSlop = ViewConfiguration.get(context).getScaledTouchSlop();
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        int action = ev.getAction();
+        switch (action) {
+            case MotionEvent.ACTION_DOWN:
+                // 记录手指按下的位置
+                mStartY = ev.getY();
+                mStartX = ev.getX();
+                // 初始化标记
+                mIsDrag = false;
+                break;
+            case MotionEvent.ACTION_MOVE:
+                // 如果viewpager正在拖拽中，那么不拦截它的事件，直接return false；
+                if (mIsDrag) {
+                    return false;
+                } 
+
+                // 获取当前手指位置
+                float endY = ev.getY();
+                float endX = ev.getX();
+                float distanceX = Math.abs(endX - mStartX);
+                float distanceY = Math.abs(endY - mStartY);
+                // 如果X轴位移大于Y轴位移，那么将事件交给viewPager处理。
+                if (distanceX > mTouchSlop && distanceX > distanceY) {
+                    mIsDrag = true;
+                    return false;
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_CANCEL:
+                // 初始化标记
+                mIsDrag = false;
+                break;
+        }
+        // 如果是Y轴位移大于X轴，事件交给swipeRefreshLayout处理。
+        return super.onInterceptTouchEvent(ev);
+    }
+}
