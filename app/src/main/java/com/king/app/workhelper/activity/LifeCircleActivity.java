f41bd0c7881c140d3057411f040b2d517dc6c44a
VanceKing <huogxu@gmail.com>
2018-03-26 13:31:59 +0800

    [test] Android Activity 生命周期

--- app/src/main/java/com/king/app/workhelper/activity/LifeCircleActivity.java
+++ app/src/main/java/com/king/app/workhelper/activity/LifeCircleActivity.java
@@ -1,13 +1,16 @@
 package com.king.app.workhelper.activity;
 
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.TextView;
 
 import com.king.app.workhelper.R;
-import com.king.app.workhelper.common.AppBaseActivity;
 
+import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 /**
@@ -15,12 +18,15 @@ import butterknife.OnClick;
  * @since 2018/1/15.
  */
 
-public class LifeCircleActivity extends AppBaseActivity {
+public class LifeCircleActivity extends AppCompatActivity {
     private static final String TAG = "aaa";
     private static final String BUNDLE_KEY = "BUNDLE_KEY";
 
     @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_life_circle);
+        ButterKnife.bind(this);
+        
         if (savedInstanceState != null) {
             Log.i(TAG, "onCreate." + savedInstanceState.getString(BUNDLE_KEY));
         } else {
@@ -33,10 +39,25 @@ public class LifeCircleActivity extends AppBaseActivity {
         Log.i(TAG, "onStart.");
     }
 
-    @Override protected int getContentLayout() {
-        return R.layout.activity_life_circle;
+    @Override protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume.");
     }
 
+    @Override protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause.");
+    }
+
+    @Override protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop.");
+    }
+
+    @Override protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onDestroy.");
+    }
 
     @Override protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
@@ -54,9 +75,19 @@ public class LifeCircleActivity extends AppBaseActivity {
         Log.i(TAG, "onSaveInstanceState");
     }
 
-    @OnClick(R.id.tv_open)
-    public void onOpenClick(TextView textView) {
-        openActivity(MainActivity.class);
+    //指定android:configChanges="orientation|screenSize"，横竖屏切换是会执行onConfigurationChanged()方法。
+    @Override public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        Log.i("aaa", "onConfigurationChanged");
     }
 
+    @OnClick(R.id.tv_open)
+    public void onOpenClick(TextView textView) {
+        openOtherActivity();
+    }
+
+    private void openOtherActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
 }
