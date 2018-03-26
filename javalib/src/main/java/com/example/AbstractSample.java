5c8d3d72b0afb424064d6c2d921de7a6d50510fa
VanceKing <huogxu@gmail.com>
2018-03-26 11:51:26 +0800

    [java] 抽象类

---
+++ javalib/src/main/java/com/example/AbstractSample.java
@@ -0,0 +1,16 @@
+package com.example;
+
+/**
+ * @author VanceKing
+ * @since 2018/3/26.
+ */
+
+abstract class AbstractSample {
+    public static void printName() {
+        System.out.println("抽象类可以没有抽象方法");
+    }
+
+    public void printName2() {
+        System.out.println("抽象类可以没有抽象方法");
+    }
+}
