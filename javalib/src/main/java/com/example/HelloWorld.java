f90c69d2ec9a7b6f4327dbc184a3db2f0c1debf8
VanceKing <huogxu@gmail.com>
2018-04-16 23:14:13 +0800

    [java] hello world

---
+++ javalib/src/main/java/com/example/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.example;
+
+/**
+ * @author VanceKing
+ * @since 2018/4/16.
+ */
+public class HelloWorld {
+    public static final String HELLO = "Hello World!";
+
+    public static void main(String[] args) {
+        System.out.println(HELLO);
+    }
+}
