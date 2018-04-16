ac9e7690c487ca0b877e12496911f927aae17851
VanceKing <huogxu@gmail.com>
2018-04-16 16:32:36 +0800

    [java] 静态变量初始化和static块初始化

--- javalib/src/main/java/com/example/ClassInitialization.java
+++ javalib/src/main/java/com/example/ClassInitialization.java
@@ -6,23 +6,38 @@ package com.example;
  */
 
 public class ClassInitialization {
-    
-    //静态初始化器：加载类时运行的的代码
-    static {
-        System.out.println("静态初始");
-    }
 
-    //实例初始化器：创建新对象时运行的代码
-    {
-        System.out.println("实例初始");
-    }
 
     public static void main(String[] args) {
-        ClassInitialization main = new ClassInitialization();
-        main.print();
+//        ClassInitialization main = new ClassInitialization();
+//        main.print();
+
+
+        System.out.println(A.COUNT);
+
     }
 
     private void print() {
         System.out.println("静态初始和实例初始的区别");
+    }
+
+    private static class A {
+
+        //同静态代码块 static{ COUNT = 6; }
+//        public static int COUNT = 6;
+        
+        //静态初始化器：加载类时运行的的代码
+        //多个static块，按先后顺序执行
+        static {
+            COUNT = 9;
+            System.out.println("静态初始");
+        }
+
+        //同静态代码块 static{ COUNT = 6; }
+        public static int COUNT = 6;
+
+        static {
+            COUNT /= 3;
+        }
     }
 }
