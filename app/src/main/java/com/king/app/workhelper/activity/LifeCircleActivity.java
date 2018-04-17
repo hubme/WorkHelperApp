ae1af07c51809e7cd92cc99ac9965105400318bb
VanceKing <huogxu@gmail.com>
2018-04-17 17:01:54 +0800

    [feature] Activity 透明主题生命周期

--- app/src/main/java/com/king/app/workhelper/activity/LifeCircleActivity.java
+++ app/src/main/java/com/king/app/workhelper/activity/LifeCircleActivity.java
@@ -3,7 +3,9 @@ package com.king.app.workhelper.activity;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.TextView;
@@ -26,7 +28,7 @@ public class LifeCircleActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_life_circle);
         ButterKnife.bind(this);
-        
+
         if (savedInstanceState != null) {
             Log.i(TAG, "onCreate." + savedInstanceState.getString(BUNDLE_KEY));
         } else {
@@ -89,11 +91,27 @@ public class LifeCircleActivity extends AppCompatActivity {
 
     @OnClick(R.id.tv_open)
     public void onOpenClick(TextView textView) {
-        openOtherActivity();
+        openActivity(MainActivity.class);
+
     }
 
-    private void openOtherActivity() {
-        Intent intent = new Intent(this, MainActivity.class);
+    //A打开透明主题的B，A不会执行onStop()方法。
+    @OnClick(R.id.tv_open_transparent)
+    public void onOpenTransparentClick(TextView textView) {
+        openActivity(TransparentActivity.class);
+    }
+
+    //不影响生命周期
+    @OnClick(R.id.tv_open_dialog)
+    public void onDialogClick(TextView textView) {
+        new AlertDialog.Builder(this)
+                .setMessage("弹窗")
+                .create()
+                .show();
+    }
+
+    protected void openActivity(@NonNull Class<?> cls) {
+        Intent intent = new Intent(this, cls);
         startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/king/app/workhelper/activity/TransparentActivity.java
@@ -0,0 +1,15 @@
+package com.king.app.workhelper.activity;
+
+import com.king.app.workhelper.R;
+import com.king.app.workhelper.common.AppBaseActivity;
+
+/**
+ * @author VanceKing
+ * @since 2018/4/17.
+ */
+
+public class TransparentActivity extends AppBaseActivity {
+    @Override protected int getContentLayout() {
+        return R.layout.activity_transparent;
+    }
+}
