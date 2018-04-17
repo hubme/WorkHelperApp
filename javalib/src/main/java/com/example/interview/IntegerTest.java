82d07c3052a686928b2b8bbabea40e550e8d6193
VanceKing <huogxu@gmail.com>
2018-04-17 09:57:58 +0800

    [java] Integer 复用

--- javalib/src/main/java/com/example/interview/IntegerTest.java
+++ javalib/src/main/java/com/example/interview/IntegerTest.java
@@ -10,7 +10,7 @@ import java.lang.reflect.Field;
 public class IntegerTest {
     public static void main(String[] args) {
         IntegerTest main = new IntegerTest();
-        main.integerSample1();
+        main.sample2();
     }
 
     /*
@@ -32,5 +32,29 @@ public class IntegerTest {
 
         }
 
+    }
+
+    //在[-128, 127]中的会返回同一个 Integer 对象
+    private void sample2() {
+        int i1 = 128;
+        //java在编译的时候,被翻译成 Integer i5 = Integer.valueOf(128);
+        Integer i2 = 128;
+        Integer i2a = 128;
+        Integer i3 = new Integer(128);
+        System.out.println(i1 == i2);//true
+        System.out.println(i1 == i3);//true
+        System.out.println(i2 == i3);//false
+        System.out.println(i2 == i2a);//false，不在缓存中，创建新的对象。
+        System.out.println("**************");
+
+        int i4 = 127;
+        Integer i5 = 127;
+        Integer i5a = 127;
+        Integer i6 = new Integer(127);
+        System.out.println(i4 == i5);//true
+        System.out.println(i4 == i6);//true
+        System.out.println(i5 == i6);//false
+        System.out.println(i5 == i5a);//true，在缓存中，复用同一个对象。
+        System.out.println("**************");
     }
 }
