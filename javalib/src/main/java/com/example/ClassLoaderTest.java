22d7a36e48e906bf7d619fa51faa32aa43e6efe2
VanceKing <huogxu@gmail.com>
2018-04-20 17:53:40 +0800

    [refactor]

--- javalib/src/main/java/com/example/ClassLoaderTest.java
+++ javalib/src/main/java/com/example/ClassLoaderTest.java
@@ -1,12 +1,13 @@
 package com.example;
 
+import java.io.InputStream;
+
 /**
  * Java类加载器的作用就是在运行时加载类。
  * Java类加载器基于三个机制：委托、可见性和单一性。
  * 委托：加载类的请求先交给父ClassLoader，如果父ClassLoader找不到或不能加载类再交给子ClassLoader加载。
  * 可见性：子ClassLoader可以看见所有的父ClassLoader加载的类，而父ClassLoader看不到子ClassLoader加载的类。
  * 单一性：一个类仅加载一次，这是由委托机制确保子ClassLoader不会再次加载父ClassLoader加载过的类。
- *
  * 三种类加载器加载类文件的地方：
  * 1) Bootstrap类加载器 – JRE/lib/rt.jar
  * 2) Extension类加载器 – JRE/lib/ext或者java.ext.dirs指向的目录
@@ -15,10 +16,61 @@ package com.example;
  */
 
 public class ClassLoaderTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        test2();
+    }
+
+    private static void test2() throws Exception {
+        ClassLoader clazzLoader = new ClassLoader() {
+            @Override
+            public Class<?> loadClass(String name) throws ClassNotFoundException {
+                return super.loadClass(name);
+                /*try {
+                    String clazzName = name.substring(name.lastIndexOf(".") + 1) + ".class";
+                    InputStream is = getClass().getResourceAsStream(clazzName);
+                    if (is == null) {//为 null 说明加载过。
+                        return super.loadClass(name);
+                    }
+                    System.out.println(name);
+                    byte[] b = new byte[is.available()];
+                    is.read(b);
+                    is.close();
+                    return defineClass(name, b, 0, b.length);
+                } catch (IOException e) {
+                    throw new ClassNotFoundException(name);
+                }*/
+            }
+
+            @Override protected Class<?> findClass(String name) throws ClassNotFoundException {
+                System.out.println("Class Name: " + name);
+                try {
+                    String clazzName = name.substring(name.lastIndexOf(".") + 1) + ".class";
+                    InputStream is = getClass().getResourceAsStream(clazzName);
+                    byte[] b = new byte[is.available()];
+                    is.read(b);
+                    is.close();
+                    return defineClass(name, b, 0, b.length);
+                } catch (Exception e) {
+                    throw new ClassNotFoundException(name);
+                }
+                
+            }
+        };
+
+        String currentClass = "com.example.ClassLoaderTest";
+        Class<?> clazz = clazzLoader.loadClass(currentClass);
+        Object obj = clazz.newInstance();
+
+        System.out.println(obj.getClass());
+        //类的全路径相同而且被同一个类加载器加载才是同一个实例。
+        System.out.println("是同一个实例吗：" + (obj instanceof ClassLoaderTest));
+
+    }
+
+    private static void test1() {
         System.out.println("ClassLoaderTest.class.getClassLoader(): " + ClassLoaderTest.class.getClassLoader());
         try {
-            Class.forName("com.example", true, ClassLoaderTest.class.getClassLoader().getParent());
+            Class.forName("com.example.ClassLoaderTest", true, ClassLoaderTest.class.getClassLoader().getParent());
         } catch (ClassNotFoundException e) {//单一性机制导致异常。
             e.printStackTrace();
         }
--- javalib/src/main/java/com/example/Main.java
+++ javalib/src/main/java/com/example/Main.java
@@ -2,35 +2,6 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
-        Main main = new Main();
-        main.testAuto();
-    }
-
-    /**
-     * 为了节省装箱产生对象消耗内存的问题，加了缓存功能。使用了享元模式(flyweight)。
-     * 享元模式:运行共享技术有效地支持大量细粒度对象的复用。
-     * 内部状态：在享元对象内部不随外界环境改变而改变的共享部分。外部状态：随着环境的改变而改变，不能够共享的状态就是外部状态。
-     * https://docs.oracle.com/javase/specs/jls/se8/html/jls-5.html#jls-5.1.7
-     * If the value p being boxed is an integer literal of type int between -128 and 127 inclusive (§3.10.1),
-     * or the boolean literal true or false (§3.10.3),
-     * or a character literal between '\u0000' and '\u007f' inclusive (§3.10.4),
-     * then let a and b be the results of any two boxing conversions of p. It is always the case that a == b.
-     */
-    private void testAuto() {
-        //[-128, 127] 会缓存。        
-        Integer int1 = -128, int2 = -128;
-        Integer int3 = -129, int4 = -129;
-        System.out.println(Boolean.toString(int1 == int2) + " " + Boolean.toString(int3 == int4));//true false
-
-        Integer int5 = 127, int6 = 127;
-        Integer int7 = 1289, int8 = 128;
-        System.out.println(Boolean.toString(int5 == int6) + " " + Boolean.toString(int7 == int8));//true false
-
-        String str1 = new String("abc");
-        String str2 = new String("abc");
-        String str3 = "abc";
-        String str4 = "abc";
-        //false true false
-        System.out.printf(Boolean.toString(str1 == str2) + " " + Boolean.toString(str3 == str4) + " " + Boolean.toString(str1 == str4));
+        
     }
 }
