7064e5e4bd948c197e5fe2bf400f10af78e780bd
VanceKing <huogxu@gmail.com>
2018-04-23 16:16:34 +0800

    [java] 深复制 浅复制

---
+++ javalib/src/main/java/com/example/Clone.java
@@ -0,0 +1,108 @@
+package com.example;
+
+/**
+ * @author VanceKing
+ * @since 2018/4/23.
+ */
+
+public class Clone {
+    public static void main(String[] args) {
+        Course course = new Course("chinese");
+
+        Student student1 = new Student("aaa", 17, course);
+        System.out.println("student1: " + student1.toString());
+
+
+        Student student2 = null;
+        try {
+            student2 = (Student) student1.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("student2: " + student2.toString());
+
+        student2.setName("bbb");
+        student2.setAge(18);
+        student2.getCourse().setName("english");
+
+        System.out.println("修改后");
+        System.out.println("student1: " + student1.toString());
+        System.out.println("student2: " + student2.toString());
+    }
+
+    private static class Student implements Cloneable {
+        private String name;
+        private int age;
+        private Course course;
+
+        public Student(String name, int age, Course course) {
+            this.name = name;
+            this.age = age;
+            this.course = course;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+        public Course getCourse() {
+            return course;
+        }
+
+        public void setCourse(Course course) {
+            this.course = course;
+        }
+
+        @Override protected Object clone() throws CloneNotSupportedException {
+            Student clone = (Student) super.clone();
+            clone.setCourse((Course) this.course.clone());
+            return clone;
+        }
+
+        @Override public String toString() {
+            return "Student{" +
+                    "name='" + name + '\'' +
+                    ", age=" + age +
+                    ", course=" + course +
+                    '}';
+        }
+    }
+
+    private static class Course implements Cloneable {
+        private String name;
+
+        public Course(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        @Override protected Object clone() throws CloneNotSupportedException {
+            return super.clone();
+        }
+
+        @Override public String toString() {
+            return "Course{" +
+                    "name='" + name + '\'' +
+                    '}';
+        }
+    }
+}
