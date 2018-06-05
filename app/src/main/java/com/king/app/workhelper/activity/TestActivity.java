aea749ec9985fef259bea814c8d983de4fbe72c8
VanceKing <huogxu@gmail.com>
2018-06-05 09:30:46 +0800

    [test] ColorStateList 使用

--- app/src/main/java/com/king/app/workhelper/activity/TestActivity.java
+++ app/src/main/java/com/king/app/workhelper/activity/TestActivity.java
@@ -1,7 +1,18 @@
 package com.king.app.workhelper.activity;
 
+import android.content.Context;
+import android.content.res.ColorStateList;
+import android.graphics.drawable.StateListDrawable;
+import android.support.v4.content.ContextCompat;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import com.king.app.workhelper.R;
 import com.king.app.workhelper.common.AppBaseActivity;
+import com.king.applib.util.ViewUtil;
+
+import butterknife.BindView;
+import butterknife.OnClick;
 
 /**
  * @author VanceKing
@@ -9,9 +20,14 @@ import com.king.app.workhelper.common.AppBaseActivity;
  */
 
 public class TestActivity extends AppBaseActivity {
+    @BindView(R.id.iv_image) ImageView imageView;
+    @BindView(R.id.tv_open_qq) TextView textView;
 
     @Override protected void initData() {
-
+        ViewUtil.setViewBackground(imageView, buildStateListDrawable(this, R.drawable.tab_home_unselect, R.drawable.tab_home_select));
+        textView.setTextColor(buildTabTitleSelector(
+                ContextCompat.getColor(this, R.color.gray_666666),
+                ContextCompat.getColor(this, R.color.chocolate)));
     }
 
     @Override protected int getContentLayout() {
@@ -22,5 +38,33 @@ public class TestActivity extends AppBaseActivity {
         super.initContentView();
     }
 
+    @OnClick(R.id.iv_image)
+    public void onImageClick(ImageView textView) {
+
+    }
+
+    @OnClick(R.id.tv_open_qq)
+    public void onQqClick(TextView textView) {
+
+    }
+
+    private StateListDrawable buildStateListDrawable(Context context, int idNormal, int idPressed) {
+        StateListDrawable drawable = new StateListDrawable();
+        drawable.addState(new int[]{android.R.attr.state_selected}, context.getResources().getDrawable(idPressed));
+        drawable.addState(new int[]{android.R.attr.state_pressed}, context.getResources().getDrawable(idPressed));
+        drawable.addState(new int[]{android.R.attr.state_enabled}, context.getResources().getDrawable(idNormal));
+        drawable.addState(new int[]{}, context.getResources().getDrawable(idNormal));
+        return drawable;
+    }
+
+    private ColorStateList buildTabTitleSelector(int normal, int pressed) {
+        int[] colors = new int[]{pressed, pressed, normal};
+        int[][] states = new int[3][];
+        states[0] = new int[]{android.R.attr.state_pressed, android.R.attr.state_enabled};
+        states[1] = new int[]{android.R.attr.state_focused, android.R.attr.state_selected};
+        states[2] = new int[]{};
+        ColorStateList colorList = new ColorStateList(states, colors);
+        return colorList;
+    }
 
 }
