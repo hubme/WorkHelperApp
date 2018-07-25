fe5b5835de387061d98896d02c6e22b32d939b23
VanceKing <huogxu@gmail.com>
2018-07-25 20:31:33 +0800

    [custom view] MaterialLoadingView

--- applib/src/main/java/com/king/applib/ui/loading/MaterialLoadingView.java
+++ applib/src/main/java/com/king/applib/ui/loading/MaterialLoadingView.java
@@ -1,13 +1,18 @@
 package com.king.applib.ui.loading;
 
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Color;
 import android.graphics.drawable.Animatable;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.view.View;
 
+import com.king.applib.R;
+
 public class MaterialLoadingView extends View {
     private Animatable mDrawable;
+    private int mColor;
 
     public MaterialLoadingView(Context context) {
         this(context, null);
@@ -19,15 +24,30 @@ public class MaterialLoadingView extends View {
 
     public MaterialLoadingView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        TypedArray typedArray = null;
+        try {
+            typedArray = context.obtainStyledAttributes(attrs, R.styleable.MaterialLoadingView);
+            mColor = typedArray.getColor(R.styleable.MaterialLoadingView_mlvColor, Color.GRAY);
+        }finally {
+            if (typedArray != null) {
+                typedArray.recycle();
+            }
+        }
         init(context);
     }
 
     private void init(Context context) {
-        setBackgroundDrawable(new MaterialLoadingDrawable(context, this));
+        MaterialProgressDrawable background = new MaterialProgressDrawable(context, this);
+        background.setColorSchemeColors(mColor);
+        background.setAlpha(255);
+        setBackgroundDrawable(background);
     }
 
     @Override
     public void setVisibility(int visibility) {
+        if (getVisibility() == visibility) {
+            return;
+        }
         if (mDrawable != null) {
             if (visibility == View.VISIBLE) {
                 mDrawable.start();
@@ -41,7 +61,7 @@ public class MaterialLoadingView extends View {
     @Override
     protected void onAttachedToWindow() {
         super.onAttachedToWindow();
-        if (mDrawable != null) {
+        if (mDrawable != null && getVisibility() == View.VISIBLE) {
             mDrawable.start();
         }
     }
