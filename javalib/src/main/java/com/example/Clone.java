b9b4126a8305aa48226f9e1547660657fd087097
VanceKing <huogxu@gmail.com>
2018-08-12 23:08:21 +0800

    [test] Cloneable

--- javalib/src/main/java/com/example/Clone.java
+++ javalib/src/main/java/com/example/Clone.java
@@ -1,24 +1,42 @@
 package com.example;
 
 /**
+ * 深拷贝和浅拷贝。原型模式。
+ * 应用场景：
+ * 1、解决构建复杂对象的资源消耗问题，提升创建对象的效率。
+ * 2、保护对象不被修改。
+ * 优点：
+ * 二进制拷贝，比 new 对象性能高。
+ * 缺点：
+ * 对象的构造方法不会执行。
+ *
  * @author VanceKing
  * @since 2018/4/23.
  */
 
 public class Clone {
-    public static void main(String[] args) {
-        Course course = new Course("chinese");
+    public static void main(String[] args) throws CloneNotSupportedException {
+        test2();
+    }
 
+    private static void test1() throws CloneNotSupportedException {
+        Course course = new Course("chinese");
+        System.out.println("course: " + course.toString());
+
+        Course clone = (Course) course.clone();
+        System.out.println("clone: " + clone.toString());
+
+        //不影响原来的对象
+        clone.setName("english");
+        System.out.println(course.toString() + " " + clone.toString());
+    }
+
+    private static void test2() throws CloneNotSupportedException {
+        Course course = new Course("chinese");
         Student student1 = new Student("aaa", 17, course);
         System.out.println("student1: " + student1.toString());
 
-
-        Student student2 = null;
-        try {
-            student2 = (Student) student1.clone();
-        } catch (CloneNotSupportedException e) {
-            e.printStackTrace();
-        }
+        Student student2 = (Student) student1.clone();
         System.out.println("student2: " + student2.toString());
 
         student2.setName("bbb");
