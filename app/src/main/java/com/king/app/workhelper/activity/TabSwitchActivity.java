7a3a1531675dd84396f5037fd6974d81d9b6f070
VanceKing <huogxu@gmail.com>
2018-08-24 15:54:22 +0800

    [feature] TabLayout

--- app/src/main/java/com/king/app/workhelper/activity/TabSwitchActivity.java
+++ app/src/main/java/com/king/app/workhelper/activity/TabSwitchActivity.java
@@ -1,8 +1,12 @@
 package com.king.app.workhelper.activity;
 
+import android.content.res.Resources;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
+import android.util.TypedValue;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import com.king.app.workhelper.R;
 import com.king.app.workhelper.adapter.viewpager.TabFragmentAdapter;
@@ -12,6 +16,7 @@ import com.king.app.workhelper.fragment.SecondTabFragment;
 import com.king.app.workhelper.fragment.SmartRefreshSampleFragment;
 import com.king.app.workhelper.fragment.ThirdTabFragment;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -66,9 +71,54 @@ public class TabSwitchActivity extends AppBaseActivity {
         mTabLayout.getTabAt(1).setText(mTitles.get(1));
         mTabLayout.getTabAt(2).setText(mTitles.get(2)).select();
         mTabLayout.getTabAt(3).setText(mTitles.get(3));
+        /*mTabLayout.post(new Runnable() {
+            @Override public void run() {
+                try {
+                    setIndicator(mTabLayout, 10, 10);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });*/
         
         /*mTabLayout.addTab(mTabLayout.newTab().setText("First"), 0, false);
         mTabLayout.addTab(mTabLayout.newTab().setText("Second"), 1, false);
         mTabLayout.addTab(mTabLayout.newTab().setText("Third"), true);*/
+    }
+
+    public void setIndicator(TabLayout tabs, int leftDip, int rightDip) {
+        Class<?> tabLayout = tabs.getClass();
+        Field tabStrip = null;
+        try {
+            tabStrip = tabLayout.getDeclaredField("mTabStrip");
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        }
+        if (tabStrip == null) {
+            return;
+        }
+        tabStrip.setAccessible(true);
+        LinearLayout llTab = null;
+        try {
+            llTab = (LinearLayout) tabStrip.get(tabs);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        if (llTab == null) {
+            return;
+        }
+
+        int left = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, leftDip, Resources.getSystem().getDisplayMetrics());
+        int right = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, rightDip, Resources.getSystem().getDisplayMetrics());
+
+        for (int i = 0; i < llTab.getChildCount(); i++) {
+            View child = llTab.getChildAt(i);
+            child.setPadding(0, 0, 0, 0);
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.MATCH_PARENT, 1);
+            params.leftMargin = left;
+            params.rightMargin = right;
+            child.setLayoutParams(params);
+            child.invalidate();
+        }
     }
 }
