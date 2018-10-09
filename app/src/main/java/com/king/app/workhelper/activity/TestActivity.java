69fc03382ebfa91acdbfa6e89b0b9f7fc075edd5
VanceKing <huogxu@gmail.com>
2018-10-09 17:37:10 +0800

    [test] ImageView scaleType

--- app/src/main/java/com/king/app/workhelper/activity/TestActivity.java
+++ app/src/main/java/com/king/app/workhelper/activity/TestActivity.java
@@ -1,13 +1,7 @@
 package com.king.app.workhelper.activity;
 
-import android.os.SystemClock;
-import android.util.Log;
-
 import com.king.app.workhelper.R;
 import com.king.app.workhelper.common.AppBaseActivity;
-import com.king.applib.util.ThreadPoolUtil;
-
-import butterknife.OnClick;
 
 /**
  * @author VanceKing
@@ -30,18 +24,6 @@ public class TestActivity extends AppBaseActivity {
     @Override protected void initContentView() {
         super.initContentView();
 
-
-    }
-
-    @OnClick(R.id.textView)
-    public void onViewClick() {
-        ThreadPoolUtil.execute(new Runnable() {
-            @Override public void run() {
-                Log.i("aaa", "job begin!");
-                SystemClock.sleep(5000);
-                Log.i("aaa", "job done!");
-            }
-        });
     }
 
 }
