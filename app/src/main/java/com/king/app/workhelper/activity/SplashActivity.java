c31b404e3c6bdcd68cbf7fc939f1667334dca9b7
VanceKing <huogxu@gmail.com>
2019-03-02 23:36:02 +0800

    [feature] 沉浸

--- app/src/main/java/com/king/app/workhelper/activity/SplashActivity.java
+++ app/src/main/java/com/king/app/workhelper/activity/SplashActivity.java
@@ -2,6 +2,7 @@ package com.king.app.workhelper.activity;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
 import android.view.View;
 import android.widget.TextView;
 
@@ -67,6 +68,23 @@ public class SplashActivity extends AppBaseActivity {
 
         startCountDown(2, 0);
         setViewClickListeners(mSplashImage, mSkipTv);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus && Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            View decorView = getWindow().getDecorView();
+            int options = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+            decorView.setSystemUiVisibility(options);
+//            getWindow().setStatusBarColor(Color.TRANSPARENT);
+//            getWindow().setNavigationBarColor(Color.TRANSPARENT);
+        }
     }
 
     @Override public void onClick(View v) {
