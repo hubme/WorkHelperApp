279365f74bfa9dd90fd6c288ee44c105e12fcece
VanceKing <huogxu@gmail.com>
2020-07-30 10:10:07 +0800

    [feature] Kotlin 扩展方法

---
+++ applib/src/main/java/com/king/applib/util/Exts.kt
@@ -0,0 +1,19 @@
+package com.king.applib.util
+
+import android.content.res.Resources
+import android.util.TypedValue
+
+/**
+ * Kotlin 扩展方法。
+ *
+ * @author guangxu.huo
+ * @since 2020/7/30
+ */
+
+val Float.dp
+    get() = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, this,
+        Resources.getSystem().displayMetrics)
+
+val Float.sp
+    get() = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, this,
+        Resources.getSystem().displayMetrics)
