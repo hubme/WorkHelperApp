169c69983ee972dfd10691565189a984b4fb1cfd
VanceKing <huogxu@gmail.com>
2020-08-05 19:03:42 +0800

    [feature] 自定义 FlowLayout

--- app/src/main/java/com/king/app/workhelper/activity/TestActivity.kt
+++ app/src/main/java/com/king/app/workhelper/activity/TestActivity.kt
@@ -2,9 +2,13 @@ package com.king.app.workhelper.activity
 
 import android.app.Activity
 import android.content.Intent
+import android.graphics.Color
+import android.view.Gravity
+import android.view.ViewGroup
+import android.widget.TextView
 import com.king.app.workhelper.R
 import com.king.app.workhelper.common.AppBaseActivity
-import com.king.app.workhelper.common.FirebaseManager
+import com.king.applib.util.dp
 import kotlinx.android.synthetic.main.activity_test.*
 
 /**
@@ -25,10 +29,40 @@ class TestActivity : AppBaseActivity() {
     override fun initContentView() {
         super.initContentView()
         tv_permission.setOnClickListener { onPermissionClick() }
+        tv_clear.setOnClickListener { flow_layout.removeAllViews() }
+        with(flow_layout) {
+            setPadding(10, 10, 30, 10)
+            setVisibleCount(3, 40f.dp)
+            setHorizontalSpacing(8)
+            setVerticalSpacing(10f.dp)
+        }
+        tv_remove_first.setOnClickListener {
+            if (flow_layout.childCount > 0) {
+                flow_layout.removeViewAt(0)
+            }
+        }
+        tv_remove_last.setOnClickListener {
+            if (flow_layout.childCount > 0) {
+                flow_layout.removeViewAt(flow_layout.childCount - 1)
+            }
+        }
     }
 
     private fun onPermissionClick() {
-        FirebaseManager.getInstance(this).logEvent("CustomEvent")
+        flow_layout.addView(buildChildView(et_added.text.toString()))
+    }
+
+    private fun buildChildView(name: String) = TextView(this).apply {
+        layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT)
+        layoutParams.height = 40f.dp
+        if (!et_view_width.text.isNullOrBlank()) {
+            layoutParams.width = et_view_width.text.toString().toInt()
+        }
+        text = name
+        setTextColor(Color.BLACK)
+        textSize = 18f
+        gravity = Gravity.CENTER
     }
 
     //reified 关键字来表示该泛型要进行实化
--- app/src/main/java/com/king/app/workhelper/ui/customview/FlowLayout.java
+++ app/src/main/java/com/king/app/workhelper/ui/customview/FlowLayout.java
@@ -2,19 +2,29 @@ package com.king.app.workhelper.ui.customview;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.core.view.ViewCompat;
+
 /**
- * 可动态换行的布局容器
+ * 可动态换行的布局容器。只支持 ChildView 高度固定。
  *
  * @author VanceKing
  * @since 2017/1/11.
  */
-
 public class FlowLayout extends ViewGroup {
-    private float mVerticalSpacing; //每个item纵向间距
-    private float mHorizontalSpacing; //每个item横向间距
+    private int mVisibleCount;
+    private int mItemHeight;
+    //子 View 一共占几行
+    private int mTotalLineCount;
+
+    private int mVerticalSpacing; //每个item纵向间距
+    private int mHorizontalSpacing; //每个item横向间距
+
+    private int mBreakChildIndex = -1;
+    private boolean mIsBreak;
 
     public FlowLayout(Context context) {
         super(context);
@@ -24,11 +34,11 @@ public class FlowLayout extends ViewGroup {
         super(context, attrs);
     }
 
-    public void setHorizontalSpacing(float pixelSize) {
+    public void setHorizontalSpacing(int pixelSize) {
         mHorizontalSpacing = pixelSize;
     }
 
-    public void setVerticalSpacing(float pixelSize) {
+    public void setVerticalSpacing(int pixelSize) {
         mVerticalSpacing = pixelSize;
     }
 
@@ -36,72 +46,142 @@ public class FlowLayout extends ViewGroup {
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         int selfWidth = resolveSize(0, widthMeasureSpec);
 
-        int paddingLeft = getPaddingLeft();
+        boolean isLtr = ViewCompat.getLayoutDirection(this) == ViewCompat.LAYOUT_DIRECTION_LTR;
+
+        int paddingStart = getPaddingStart();
         int paddingTop = getPaddingTop();
-        int paddingRight = getPaddingRight();
+        int paddingEnd = getPaddingEnd();
         int paddingBottom = getPaddingBottom();
 
-        int childLeft = paddingLeft;
-        int childTop = paddingTop;
-        int lineHeight = 0;
+        int offsetStart = isLtr ? paddingStart : selfWidth - paddingStart;
+        int offsetTop = paddingTop;
+        int availableMaxWidth = selfWidth - paddingEnd;
 
-        //通过计算每一个子控件的高度，得到自己的高度 
-        for (int i = 0, childCount = getChildCount(); i < childCount; ++i) {
+        mIsBreak = false;
+        mBreakChildIndex = -1;
+        mTotalLineCount = 0;
+        final int childCount = getChildCount();
+        for (int i = 0; i < childCount; i++) {
             View childView = getChildAt(i);
-            LayoutParams childLayoutParams = childView.getLayoutParams();
-            childView.measure(getChildMeasureSpec(widthMeasureSpec, paddingLeft + paddingRight, childLayoutParams.width),
-                    getChildMeasureSpec(heightMeasureSpec, paddingTop + paddingBottom, childLayoutParams.height));
+            LayoutParams params = (LayoutParams) childView.getLayoutParams();
+            childView.measure(
+                    getChildMeasureSpec(widthMeasureSpec, paddingStart + paddingEnd, params.width),
+                    getChildMeasureSpec(heightMeasureSpec, paddingTop + paddingBottom,
+                            params.height));
             int childWidth = childView.getMeasuredWidth();
-            int childHeight = childView.getMeasuredHeight();
 
-            lineHeight = Math.max(childHeight, lineHeight);
+            if (isLtr) {
+                if (offsetStart + childWidth > availableMaxWidth) {//超过父 View 宽度，换行，行数 +1
+                    offsetStart = paddingStart;
+                    offsetTop += (mItemHeight + mVerticalSpacing);
+                    mTotalLineCount++;
+                }
 
-            if (childLeft + childWidth + paddingRight > selfWidth) {
-                childLeft = paddingLeft;
-                childTop += mVerticalSpacing + lineHeight;
-                lineHeight = childHeight;
+                params.x = offsetStart;
+                params.y = offsetTop;
+
+                offsetStart += (childWidth + mHorizontalSpacing);
             } else {
-                childLeft += childWidth + mHorizontalSpacing;
+                if (offsetStart - childWidth < 0) {//超过父 View 宽度，换行，行数 +1
+                    offsetStart = selfWidth - paddingStart;
+                    offsetTop += (mItemHeight + mVerticalSpacing);
+                    mTotalLineCount++;
+                }
+                params.x = offsetStart - childWidth;
+                params.y = offsetTop;
+                offsetStart -= (childWidth + mHorizontalSpacing);
+
+            }
+
+            if (mVisibleCount > 0 && mTotalLineCount == mVisibleCount) {
+                mBreakChildIndex = i - 1;
+                mIsBreak = true;
+                break;
             }
         }
+        //处理占据一行但是不满一行的情况
+        if (isLtr) {
+            if (offsetStart > paddingStart && offsetStart <= availableMaxWidth) {
+                mTotalLineCount += 1;
+            }
+        } else {
+            if (offsetStart + mHorizontalSpacing >= 0 && offsetStart + mHorizontalSpacing < selfWidth - paddingStart) {
+                mTotalLineCount += 1;
+            }
+        }
+        Log.i("aaa", mTotalLineCount + "行");
 
-        int wantedHeight = childTop + lineHeight + paddingBottom;
-        setMeasuredDimension(selfWidth, resolveSize(wantedHeight, heightMeasureSpec));
+        if (mVisibleCount > 0 && mVisibleCount < mTotalLineCount) {
+            //不包含最后一行下面的 mVerticalSpacing
+            int wantedHeight = paddingTop + paddingBottom + (mItemHeight + mVerticalSpacing) * mVisibleCount - mVerticalSpacing;
+            setMeasuredDimension(selfWidth, resolveSize(wantedHeight, heightMeasureSpec));
+        } else {
+            int wantedHeight = paddingTop + paddingBottom + (mItemHeight + mVerticalSpacing) * mTotalLineCount - mVerticalSpacing;
+            setMeasuredDimension(selfWidth, resolveSize(wantedHeight, heightMeasureSpec));
+        }
+    }
+
+    @Override
+    protected boolean checkLayoutParams(ViewGroup.LayoutParams p) {
+        return p instanceof FlowLayout.LayoutParams;
+    }
+
+    @Override
+    protected FlowLayout.LayoutParams generateDefaultLayoutParams() {
+        return new FlowLayout.LayoutParams(FlowLayout.LayoutParams.WRAP_CONTENT,
+                FlowLayout.LayoutParams.WRAP_CONTENT);
+    }
+
+    @Override
+    public FlowLayout.LayoutParams generateLayoutParams(AttributeSet attrs) {
+        return new FlowLayout.LayoutParams(getContext(), attrs);
+    }
+
+    @Override
+    protected FlowLayout.LayoutParams generateLayoutParams(ViewGroup.LayoutParams p) {
+        return new FlowLayout.LayoutParams(p.width, p.height);
     }
 
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
-        int myWidth = r - l;
-
-        int paddingLeft = getPaddingLeft();
-        int paddingTop = getPaddingTop();
-        int paddingRight = getPaddingRight();
-
-        int childLeft = paddingLeft;
-        int childTop = paddingTop;
-
-        int lineHeight = 0;
-
-        //根据子控件的宽高，计算子控件应该出现的位置。 
-        for (int i = 0, childCount = getChildCount(); i < childCount; ++i) {
-            View childView = getChildAt(i);
-
-            if (childView.getVisibility() == View.GONE) {
-                continue;
-            }
-
-            int childWidth = childView.getMeasuredWidth();
-            int childHeight = childView.getMeasuredHeight();
-
-            lineHeight = Math.max(childHeight, lineHeight);
-
-            if (childLeft + childWidth + paddingRight > myWidth) {
-                childLeft = paddingLeft;
-                childTop += mVerticalSpacing + lineHeight;
-                lineHeight = childHeight;
-            }
-            childView.layout(childLeft, childTop, childLeft + childWidth, childTop + childHeight);
-            childLeft += childWidth + mHorizontalSpacing;
+        int index = mIsBreak ? mBreakChildIndex : getChildCount() - 1;
+        for (int i = 0; i <= index; i++) {
+            View child = getChildAt(i);
+            FlowLayout.LayoutParams lp = (FlowLayout.LayoutParams) child.getLayoutParams();
+            child.layout(lp.x, lp.y, lp.x + child.getMeasuredWidth(),
+                    lp.y + child.getMeasuredHeight());
         }
+    }
+
+    public static class LayoutParams extends ViewGroup.LayoutParams {
+        int x;
+        int y;
+
+        public LayoutParams(Context c, AttributeSet attrs) {
+            super(c, attrs);
+        }
+
+        public LayoutParams(int width, int height) {
+            super(width, height);
+        }
+
+        public LayoutParams(ViewGroup.LayoutParams source) {
+            super(source);
+        }
+    }
+
+    public void setVisibleCount(int count, int childHeight) {
+        if (mVisibleCount != count) {
+            mVisibleCount = count;
+            mItemHeight = childHeight;
+            requestLayout();
+        }
+    }
+
+    /**
+     * 所有的子 View 一共占据多少行。
+     */
+    public int getTotalCount() {
+        return mTotalLineCount;
     }
 }
