afbd2d5f901bcc7fe7b7643c3d0e3eb040db947c
VanceKing <huogxu@gmail.com>
2020-10-13 17:20:37 +0800

    [feature] Activity#onUserLeaveHint()

--- app/src/main/java/com/king/app/workhelper/activity/TestActivity.kt
+++ app/src/main/java/com/king/app/workhelper/activity/TestActivity.kt
@@ -3,13 +3,13 @@ package com.king.app.workhelper.activity
 import android.app.Activity
 import android.content.Intent
 import android.graphics.Color
-import android.view.Gravity
-import android.view.ViewGroup
-import android.widget.TextView
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.TransitionDrawable
 import com.king.app.workhelper.R
 import com.king.app.workhelper.common.AppBaseActivity
-import com.king.applib.util.dp
+import com.king.applib.log.Logger
 import kotlinx.android.synthetic.main.activity_test.*
+
 
 /**
  * @author VanceKing
@@ -28,41 +28,18 @@ class TestActivity : AppBaseActivity() {
 
     override fun initContentView() {
         super.initContentView()
-        tv_permission.setOnClickListener { onPermissionClick() }
-        tv_clear.setOnClickListener { flow_layout.removeAllViews() }
-        with(flow_layout) {
-            setPadding(10, 10, 30, 10)
-            setVisibleCount(3, 40f.dp)
-            setHorizontalSpacing(8)
-            setVerticalSpacing(10f.dp)
-        }
-        tv_remove_first.setOnClickListener {
-            if (flow_layout.childCount > 0) {
-                flow_layout.removeViewAt(0)
-            }
-        }
-        tv_remove_last.setOnClickListener {
-            if (flow_layout.childCount > 0) {
-                flow_layout.removeViewAt(flow_layout.childCount - 1)
-            }
-        }
+        val text = "狂赛季疯狂是反对龙kjdfoiwejflsd龙井扥塞拉芬零件sdklfjiowejfslef双看扥令赛季反对s神霜扥sljsfsdsdlkfjsldf狂赛季疯狂是反对龙kjdfoiwejflsd龙井扥塞拉芬零件sdklfjiowejfslef双看扥令赛季反对s神霜扥sljsfsdsdlkfjsldf"
+
+        val transitionDrawable = TransitionDrawable(arrayOf(ColorDrawable(Color.RED), ColorDrawable(Color.GREEN), ColorDrawable(Color.BLUE)))
+        tv_text.text = text
+        tv_text.background = transitionDrawable
+        tv_text.setOnClickListener { transitionDrawable.startTransition(300) }
     }
 
-    private fun onPermissionClick() {
-        flow_layout.addView(buildChildView(et_added.text.toString()))
-    }
-
-    private fun buildChildView(name: String) = TextView(this).apply {
-        layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
-            ViewGroup.LayoutParams.WRAP_CONTENT)
-        layoutParams.height = 40f.dp
-        if (!et_view_width.text.isNullOrBlank()) {
-            layoutParams.width = et_view_width.text.toString().toInt()
-        }
-        text = name
-        setTextColor(Color.BLACK)
-        textSize = 18f
-        gravity = Gravity.CENTER
+    //由于用户操作，应用进入后台时(home 键)调用，在 onPause() 之前执行
+    override fun onUserLeaveHint() {
+        super.onUserLeaveHint()
+        Logger.i("onUserLeaveHint")
     }
 
     //reified 关键字来表示该泛型要进行实化
