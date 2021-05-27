4177456c961fa75719024030e4fcef7ba17b94c3
VanceKing <huogxu@gmail.com>
2021-05-27 10:35:57 +0800

    http status code

--- app/src/main/java/com/king/app/workhelper/retrofit/exception/ApiException.java
+++ app/src/main/java/com/king/app/workhelper/retrofit/exception/ApiException.java
@@ -1,7 +1,8 @@
 package com.king.app.workhelper.retrofit.exception;
 
 /**
- * 公共的请求错误处理类
+ * 公共的请求错误处理类。
+ * https://developer.mozilla.org/zh-CN/docs/Web/HTTP/Status
  *
  * @author VanceKing
  * @since 2017/5/27.
@@ -24,6 +25,15 @@ public class ApiException extends RuntimeException {
 
     public static final int CODE_ERROR_MALFORMED_JSON = 1020;
     public static final String MSG_ERROR_MALFORMED_JSON = "数据解析错误";
+
+    private static final int UNAUTHORIZED = 401;
+    private static final int FORBIDDEN = 403;
+    private static final int NOT_FOUND = 404;
+    private static final int REQUEST_TIMEOUT = 408;
+    private static final int INTERNAL_SERVER_ERROR = 500;
+    private static final int BAD_GATEWAY = 502;
+    private static final int SERVICE_UNAVAILABLE = 503;
+    private static final int GATEWAY_TIMEOUT = 504;
 
     public int code;
     public String msg;
