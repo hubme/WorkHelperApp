04bd1f10e1513b378aa3cd55e373bf0f6750efc8
VanceKing <huogxu@gmail.com>
2021-09-17 19:59:42 +0800

    onSaveInstanceState() 和 onRestoreInstanceState() 调用时机

--- app/src/main/java/com/king/app/workhelper/activity/LifeCircleActivity.java
+++ app/src/main/java/com/king/app/workhelper/activity/LifeCircleActivity.java
@@ -22,7 +22,8 @@ public class LifeCircleActivity extends AppCompatActivity {
     private static final String BUNDLE_KEY = "BUNDLE_KEY";
     private boolean mIsShowFragmentLog = true;
 
-    @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_life_circle);
         ButterKnife.bind(this);
@@ -40,13 +41,16 @@ public class LifeCircleActivity extends AppCompatActivity {
         }
     }
 
-    @Override protected void onStart() {
+    @Override
+    protected void onStart() {
         super.onStart();
         log("onStart.");
     }
 
+    // 在 onStart() 与 onPostCreate() 之间被调用。onPostCreate() 在 onResume() 之前调用。
     //onRestoreInstanceState被调用的前提是，activity “确实”被系统销毁了
-    @Override protected void onRestoreInstanceState(Bundle savedInstanceState) {
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
         if (savedInstanceState != null) {
             log("onRestoreInstanceState." + savedInstanceState.getString(BUNDLE_KEY));
@@ -61,41 +65,48 @@ public class LifeCircleActivity extends AppCompatActivity {
         log("onRestart.");
     }
 
-    @Override protected void onResume() {
+    @Override
+    protected void onResume() {
         super.onResume();
         log("onResume.");
     }
 
-    @Override protected void onPause() {
+    @Override
+    protected void onPause() {
         super.onPause();
         log("onPause.");
     }
 
+    // >=9.0: 在 onStop() 后调用；< 9.0: onStop() 之前，但是不确定是在 onPause() 前后。
     //将要被kill的时候回调（例如按Home进入后台、长按Home选择其他程序、锁屏、屏幕旋转前、跳转下一个Activity等情况下会被调用）
     //可以打开开发者选项中的“不保留活动”调试。主动关闭 Activity 不会调用该方法。
     //onSaveInstanceState的调用遵循一个重要原则，即当系统“未经你许可”时销毁了你的activity，则onSaveInstanceState会被系统调用，
     //因为系统有责任也应该提供机会让你保存数据。
     //see also: android.view.View.onSaveInstanceState()
-    @Override protected void onSaveInstanceState(Bundle outState) {
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
         //注意在调用 super 之前保存数据
         outState.putString(BUNDLE_KEY, "哈哈哈");
         super.onSaveInstanceState(outState);
         log("onSaveInstanceState");
     }
 
-    @Override protected void onStop() {
+    @Override
+    protected void onStop() {
         super.onStop();
         log("onStop.");
     }
 
-    @Override protected void onDestroy() {
+    @Override
+    protected void onDestroy() {
         super.onDestroy();
         log("onDestroy.");
     }
 
     //Activity#isChangingConfigurations() 判断 Config 是否变化了
     //指定android:configChanges="orientation|screenSize"，横竖屏切换是会执行onConfigurationChanged()方法。
-    @Override public void onConfigurationChanged(Configuration newConfig) {
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
         log("onConfigurationChanged");
     }
@@ -105,7 +116,8 @@ public class LifeCircleActivity extends AppCompatActivity {
     //ActivityThread#performDestroyActivity(..., true) -> Activity#retainNonConfigurationInstances()
     //Activity#onRetainNonConfigurationInstance() -> FragmentActivity#onRetainCustomNonConfigurationInstance()
     //Activity#NonConfigurationInstances 静态内部类
-    @Override public Object onRetainCustomNonConfigurationInstance() {
+    @Override
+    public Object onRetainCustomNonConfigurationInstance() {
         return null;//通过getLastCustomNonConfigurationInstance();
     }
 
