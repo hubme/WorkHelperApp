2f89c971a66c6b1083781d3c969c94a61667ee8d
VanceKing <huogxu@gmail.com>
2021-09-26 20:21:08 +0800

    ViewBinding 使用

--- app/src/main/java/com/king/app/workhelper/activity/TestActivity.kt
+++ app/src/main/java/com/king/app/workhelper/activity/TestActivity.kt
@@ -4,12 +4,10 @@ import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.text.Annotation
-import com.king.app.workhelper.R
+import android.view.View
 import com.king.app.workhelper.common.AppBaseActivity
-import com.king.app.workhelper.common.FirebaseManager
+import com.king.app.workhelper.databinding.ActivityTestBinding
 import com.king.applib.log.Logger
-import com.king.applib.util.EncryptionUtil
-import kotlinx.android.synthetic.main.activity_test.*
 
 
 /**
@@ -19,8 +17,11 @@ import kotlinx.android.synthetic.main.activity_test.*
 
 class TestActivity : AppBaseActivity() {
 
-    override fun getContentLayout(): Int {
-        return R.layout.activity_test
+    private lateinit var mBinding: ActivityTestBinding
+
+    override fun getContentView(): View {
+        mBinding = ActivityTestBinding.inflate(layoutInflater)
+        return mBinding.root
     }
 
     override fun initData() {
@@ -30,28 +31,7 @@ class TestActivity : AppBaseActivity() {
     override fun initContentView() {
         super.initContentView()
 
-        val message = "abc123456"
-
-
-        btn_des.setOnClickListener {
-            FirebaseManager.getInstance(this).logEvent("MyEvent")
-        }
-
-        btn_aes.setOnClickListener {
-            val aesKey = "@ng!@#4)8l;Jlk9$"
-            val encryptText = EncryptionUtil.encryptByAES(message, aesKey)
-            val plantText = EncryptionUtil.decryptByAES(encryptText, aesKey)
-            Logger.i("密文：$encryptText \n 明文：$plantText")
-        }
-
-        val algorithm = "RSA"
-        val keyPair = EncryptionUtil.generateRSAKeyPair(algorithm, 2048)
-        btn_rsa.setOnClickListener {
-
-            val encryptText = EncryptionUtil.encryptByRSA(algorithm, keyPair.public, message)
-            val plantText = EncryptionUtil.decryptByRSA(algorithm, keyPair.private, encryptText)
-            Logger.i("密文：$encryptText \n 明文：$plantText")
-        }
+        mBinding.tvText.text = "哈哈哈"
     }
 
     //由于用户操作，应用进入后台时(home 键)调用，在 onPause() 之前执行
