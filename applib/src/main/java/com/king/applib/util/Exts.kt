90f4cbf40ba57836410469ff6c7d6fd19d9f1a5d
VanceKing <huogxu@gmail.com>
2021-10-22 15:24:14 +0800

    startActivity 扩展方法

--- applib/src/main/java/com/king/applib/util/Exts.kt
+++ applib/src/main/java/com/king/applib/util/Exts.kt
@@ -1,5 +1,8 @@
 package com.king.applib.util
 
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
 import android.content.SharedPreferences
 import android.content.res.Resources
 import android.util.TypedValue
@@ -12,15 +15,23 @@ import android.util.TypedValue
  */
 
 val Float.dp
-    get() = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, this,
-            Resources.getSystem().displayMetrics).toInt()
+    get() = TypedValue.applyDimension(
+        TypedValue.COMPLEX_UNIT_DIP, this,
+        Resources.getSystem().displayMetrics
+    ).toInt()
 
 val Float.sp
-    get() = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, this,
-            Resources.getSystem().displayMetrics).toInt()
+    get() = TypedValue.applyDimension(
+        TypedValue.COMPLEX_UNIT_SP, this,
+        Resources.getSystem().displayMetrics
+    ).toInt()
 
 fun SharedPreferences.spEdit(action: SharedPreferences.Editor.() -> Unit) {
     val editor = edit()
     action(editor)
     editor.apply()
+}
+
+inline fun <reified T : Activity> Context.openActivity(configIntent: Intent.() -> Unit = {}) {
+    startActivity(Intent(this, T::class.java).apply(configIntent))
 }
