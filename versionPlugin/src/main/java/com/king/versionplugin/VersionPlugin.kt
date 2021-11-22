2b7615c551770b413bc8b2a6017e456b06adf8e3
VanceKing <huogxu@gmail.com>
2021-11-22 19:35:38 +0800

    使用 Composing builds 方式统一依赖

---
+++ versionPlugin/src/main/java/com/king/versionplugin/VersionPlugin.kt
@@ -0,0 +1,20 @@
+package com.king.versionplugin
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+/**
+ * https://docs.gradle.org/current/userguide/composite_builds.html
+ *
+ * @author VanceKing
+ * @since 2021/11/22
+ */
+class VersionPlugin : Plugin<Project> {
+    override fun apply(project: Project) {
+
+    }
+
+    companion object {
+
+    }
+}
