760d7a6724b7ba8adf44c4b721b1a71f94ce3af2
VanceKing <huogxu@gmail.com>
2022-04-09 17:25:25 +0800

    设置 Glide 使用 RGB_565 模式加载图片

--- app/src/main/java/com/king/app/workhelper/CustomGlideModule.java
+++ app/src/main/java/com/king/app/workhelper/CustomGlideModule.java
@@ -2,14 +2,17 @@ package com.king.app.workhelper;
 
 import android.content.Context;
 
+import androidx.annotation.NonNull;
+
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.GlideBuilder;
 import com.bumptech.glide.Registry;
 import com.bumptech.glide.annotation.GlideModule;
 import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader;
+import com.bumptech.glide.load.DecodeFormat;
 import com.bumptech.glide.load.model.GlideUrl;
 import com.bumptech.glide.module.AppGlideModule;
-
-import org.jetbrains.annotations.NotNull;
+import com.bumptech.glide.request.RequestOptions;
 
 import java.io.InputStream;
 import java.util.concurrent.TimeUnit;
@@ -24,7 +27,7 @@ import okhttp3.OkHttpClient;
 @GlideModule
 public class CustomGlideModule extends AppGlideModule {
     @Override
-    public void registerComponents(@NotNull Context context, Glide glide, @NotNull Registry registry) {
+    public void registerComponents(@NonNull Context context, Glide glide, @NonNull Registry registry) {
         OkHttpClient client = new OkHttpClient.Builder()
                 .connectTimeout(15, TimeUnit.SECONDS)
                 .readTimeout(15, TimeUnit.SECONDS)
@@ -33,6 +36,14 @@ public class CustomGlideModule extends AppGlideModule {
 
         OkHttpUrlLoader.Factory factory = new OkHttpUrlLoader.Factory((Call.Factory) client);
         glide.getRegistry().replace(GlideUrl.class, InputStream.class, factory);
+    }
+
+    @Override
+    public void applyOptions(@NonNull Context context, @NonNull GlideBuilder builder) {
+        super.applyOptions(context, builder);
+
+        //在Glide内部，会读取原始图片的EXIF头信息，获取当前图片的格式。若当前格式的图片支持alpha通道，则还是会设置为ARGB_8888的格式。
+        builder.setDefaultRequestOptions(new RequestOptions().format(DecodeFormat.PREFER_RGB_565));
     }
 
     @Override
